94d59c090f826c1ec3843237f77fe3acf0d26c9d
Sergej Kern <sk@roboterwerke.eu>
2015-09-17 14:08:36 +0200

    Update GroupUtil.h

--- TextSecureZ/src/securesms/util/GroupUtil.h
+++ TextSecureZ/src/securesms/util/GroupUtil.h
@@ -2,15 +2,20 @@
 /*
 Port of class GroupUtil from TextSecure-java
 */
-#define ENCODED_GROUP_PREFIX = "__textsecure_group__!"
+
 class GroupUtil
 {
 private:
+  const char* ENCODED_GROUP_PREFIX = "__textsecure_group__!";
+
 public:
   GroupUtil();
   virtual ~GroupUtil();
-
   static char* GetEncodedId(char* groupId);
+  char* GetDecodedId(char* groupId);
+  static bool IsEncodedGroup(char* groupId);
+  // static char* GetDescription(Context context, String encodedGroup);
+  static char* GetDescription(void* context, char* encodedGroup);
 };
 
 // TODO
