222ec46fd731c87f240af6f2f9cd8636f51076cc
Sergej Kern <sk@roboterwerke.eu>
2015-09-18 16:32:15 +0200

    Update GroupUtil.h

--- securesms/util/GroupUtil.h
+++ securesms/util/GroupUtil.h
@@ -3,6 +3,9 @@
 Port of class GroupUtil from TextSecure-java
 */
 
+// [ ] done
+// TFS ID: 222
+
 class GroupUtil
 {
 private:
@@ -13,9 +16,7 @@ public:
   virtual ~GroupUtil();
   static char* GetEncodedId(char* groupId);
   char* GetDecodedId(char* groupId);
-  static bool IsEncodedGroup(char* groupId);
-  // static char* GetDescription(Context context, String encodedGroup);
+  static bool IsEncodedGroup(unsigned char* groupId);
+  // TODO OS SPECIFIC static char* GetDescription(Context context, String encodedGroup);
   static char* GetDescription(void* context, char* encodedGroup);
 };
-
-// TODO
