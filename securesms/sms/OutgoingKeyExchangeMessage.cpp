0181513ee056ef2f89d6e544f5737a65f1ca3c1d
Sergej Kern <sk@roboterwerke.eu>
2015-09-18 16:37:41 +0200

    Update OutgoingKeyExchangeMessage.cpp

--- securesms/sms/OutgoingKeyExchangeMessage.cpp
+++ securesms/sms/OutgoingKeyExchangeMessage.cpp
@@ -1,11 +1,20 @@
 #include "OutgoingKeyExchangeMessage.h"
 
 
-OutgoingKeyExchangeMessage::OutgoingKeyExchangeMessage()
+OutgoingKeyExchangeMessage::OutgoingKeyExchangeMessage(OutgoingKeyExchangeMessage* base, char* body) : OutgoingTextMessage(base, body)
 {
 }
-
-
 OutgoingKeyExchangeMessage::~OutgoingKeyExchangeMessage()
+{
+}
+OutgoingKeyExchangeMessage::OutgoingKeyExchangeMessage(Recipients* recipients, char* message) : OutgoingTextMessage(recipients, message)
+{
+}
+//@Override
+bool OutgoingKeyExchangeMessage::IsKeyExchange()
+{
+}
+//@Override
+OutgoingTextMessage* OutgoingKeyExchangeMessage::WithBody(char* body)
 {
 }
