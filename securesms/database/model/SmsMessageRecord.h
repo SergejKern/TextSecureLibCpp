f4344fb6a306ea019f9c68b64a0a0a8978308c3a
Sergej Kern <sk@roboterwerke.eu>
2015-09-18 10:47:59 +0200

    Create SmsMessageRecord.h

---
+++ securesms/database/model/SmsMessageRecord.h
@@ -0,0 +1,16 @@
+#pragma once
+
+/*
+Port of SmsMessageRecord from TextSecure-android
+*/
+
+#include "MessageRecord.h"
+
+class SmsMessageRecord :
+  public MessageRecord
+{
+public:
+  virtual ~SmsMessageRecord();
+
+
+};
