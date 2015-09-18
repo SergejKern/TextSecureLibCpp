a9330a94763402a7125da3881df7f9fdc7a4e4b3
Sergej Kern <sk@roboterwerke.eu>
2015-09-18 16:36:31 +0200

    Update OutgoingEndSessionMessage.cpp

--- securesms/sms/OutgoingEndSessionMessage.cpp
+++ securesms/sms/OutgoingEndSessionMessage.cpp
@@ -1,11 +1,24 @@
 #include "OutgoingEndSessionMessage.h"
 
-
-OutgoingEndSessionMessage::OutgoingEndSessionMessage()
-{
-}
-
+// [x] done
+// TFS ID: 
 
 OutgoingEndSessionMessage::~OutgoingEndSessionMessage()
 {
+}
+OutgoingEndSessionMessage::OutgoingEndSessionMessage(OutgoingTextMessage* base) : OutgoingTextMessage(base)
+{
+}
+OutgoingEndSessionMessage::OutgoingEndSessionMessage(OutgoingTextMessage* message, char* body) : OutgoingTextMessage(message, body)
+{
+}
+//@Override
+bool OutgoingEndSessionMessage::IsEndSession()
+{
+  return true;
+}
+//@Override
+OutgoingTextMessage* OutgoingEndSessionMessage::WithBody(char* body)
+{
+  return new OutgoingEndSessionMessage(this, body);
 }
