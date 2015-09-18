af4dc3bba4945e54d02e2f93ec92085ad4ea6b10
Sergej Kern <sk@roboterwerke.eu>
2015-09-18 16:35:10 +0200

    Update OutgoingEncryptedMessage.cpp

--- securesms/sms/OutgoingEncryptedMessage.cpp
+++ securesms/sms/OutgoingEncryptedMessage.cpp
@@ -1,11 +1,26 @@
 #include "OutgoingEncryptedMessage.h"
 
-
-OutgoingEncryptedMessage::OutgoingEncryptedMessage()
-{
-}
-
+// [x] done
+// TFS ID: 
 
 OutgoingEncryptedMessage::~OutgoingEncryptedMessage()
 {
+}
+
+OutgoingEncryptedMessage::OutgoingEncryptedMessage(Recipients* recipients, char* body) : OutgoingTextMessage(recipients, body)
+{
+}
+
+OutgoingEncryptedMessage::OutgoingEncryptedMessage(OutgoingEncryptedMessage* base, char* body) : OutgoingTextMessage(base, body)
+{
+}
+//@Override
+bool OutgoingEncryptedMessage::IsSecureMessage()
+{
+  return true;
+}
+//@Override
+OutgoingTextMessage* OutgoingEncryptedMessage::WithBody(char* body)
+{
+  return new OutgoingEncryptedMessage(this, body);
 }
