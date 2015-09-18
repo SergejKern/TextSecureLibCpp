2e413dbbf6bb72d4765944ca5852c0d7656518b0
Sergej Kern <sk@roboterwerke.eu>
2015-09-18 11:16:05 +0200

    Update TextSecureGroup.cpp

--- lib/api/messages/TextSecureGroup.cpp
+++ lib/api/messages/TextSecureGroup.cpp
@@ -1,128 +1,50 @@
-#pragma once
-
 /*
 Port of TextSecureGroup.java from libtextsecure-java
 */
 
-/**
-* Copyright (C) 2014 Open Whisper Systems
-*
-* This program is free software: you can redistribute it and/or modify
-* it under the terms of the GNU General Public License as published by
-* the Free Software Foundation, either version 3 of the License, or
-* (at your option) any later version.
-*
-* This program is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-* GNU General Public License for more details.
-*
-* You should have received a copy of the GNU General Public License
-* along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
+#include "TextSecureGroup.h"
 
-//import org.whispersystems.libaxolotl.util.guava.Optional;
-
-/**
-* Group information to include in TextSecureMessages destined to groups.
-*
-* This class represents a "context" that is included with textsecure messages
-* to make them group messages.  There are three types of context:
-*
-* 1) Update -- Sent when either creating a group, or updating the properties
-*    of a group (such as the avatar icon, membership list, or title).
-* 2) Deliver -- Sent when a message is to be delivered to an existing group.
-* 3) Quit -- Sent when the sender wishes to leave an existing group.
-*
-* @author Moxie Marlinspike
-*/
-
-#if (_MSC_VER > 1500)
-#define NULL nullptr
-#endif
-
-#include "TextSecureAttachment.h"
-
-class TextSecureGroup
+TextSecureGroup::TextSecureGroup(char* groupId)
 {
-public:
-  enum Type {
-    UNKNOWN,
-    UPDATE,
-    DELIVER,
-    QUIT
-  };
-  static class Builder
-  {
-  private:
-    Type type;
-    char* id;
-    char* name;
-    char** members;
-    TextSecureAttachment* avatar;
-  public:
-    // constructor
-    Builder(Type type) {
-      this->type = type;
-    }
-    Builder* WithId(char* id)
-    {
-      this->id = id;
-      return this;
-    }
-    Builder* WithName(char* name)
-    {
-      this->name = name;
-      return this;
-    }
-    Builder* WithMembers(char** members)
-    {
-      this->members = members;
-      return this;
-    }
-    Builder* WithAvatar(TextSecureAttachment* avatar) {
-      this->avatar = avatar;
-      return this;
-    }
-    TextSecureGroup* Build()
-    {
-      if (id == nullptr)
-      {
-        // do not throw exceptions in TIZEN
-        // throw new IllegalArgumentException("No group ID specified!");
-      }
-
-      if (type == Type::UPDATE && name == NULL && members == NULL && avatar == NULL)
-      {
-        // do not throw exceptions in TIZEN
-        //throw new IllegalArgumentException("Group update with no updates!");
-      }
-
-      return new TextSecureGroup(type, id, name, members, avatar);
-    }
-  };
-  // constructors
-  TextSecureGroup(char* groupId);
-  TextSecureGroup(Type type, char* groupId, char* name, char** members, TextSecureAttachment* avatar);
-  // destructor
-  virtual ~TextSecureGroup();
-  // methods
-  char* GetGroupId();
-  Type GetType();
-  char* GetName();
-  char** GetMembers();
-  TextSecureAttachment* GetAvatar();
-  static Builder* NewUpdateBuilder();
-  static Builder* NewBuilder(Type type);
-
-private:
-  char* groupId;
-  Type type;
-  //Optional<String> name;
-  char* name;
-  //Optional<List<String>> members;
-  char** members;
-  //Optional<TextSecureAttachment> avatar;
-  TextSecureAttachment* avatar;
-};
-
+  // TODO: rewrite!
+  //this(Type::DELIVER, groupId, NULL, NULL, NULL);
+}
+TextSecureGroup::TextSecureGroup(Type type, char* groupId, char* name, char** members, TextSecureAttachment* avatar)
+{
+  this->type = type;
+  this->groupId = groupId;
+  // TODO
+  this->name = name;
+  // TODO
+  this->members = members;
+  // TODO
+  this->avatar = avatar;
+}
+char* TextSecureGroup::GetGroupId()
+{
+  return this->groupId;
+}
+TextSecureGroup::Type TextSecureGroup::GetType() {
+  return this->type;
+}
+char* TextSecureGroup::GetName() {
+  return this->name;
+}
+char** TextSecureGroup::GetMembers() {
+  return this->members;
+}
+TextSecureAttachment* TextSecureGroup::GetAvatar()
+{
+  return this->avatar;
+}
+TextSecureGroup::~TextSecureGroup()
+{
+}
+TextSecureGroup::Builder* TextSecureGroup::NewUpdateBuilder()
+{
+  return new TextSecureGroup::Builder(Type::UPDATE);
+}
+TextSecureGroup::Builder* TextSecureGroup::NewBuilder(Type type)
+{
+  return new TextSecureGroup::Builder(type);
+}
