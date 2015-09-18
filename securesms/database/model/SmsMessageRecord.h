c1d4673002b164d048bb66f609148bfc22046d30
Sergej Kern <sk@roboterwerke.eu>
2015-09-18 16:26:58 +0200

    Update SmsMessageRecord.h

--- securesms/database/model/SmsMessageRecord.h
+++ securesms/database/model/SmsMessageRecord.h
@@ -6,6 +6,9 @@ Port of SmsMessageRecord from TextSecure-android
 
 #include "MessageRecord.h"
 
+// [ ] done
+// TFS ID: 304
+
 class SmsMessageRecord :
   public MessageRecord
 {
@@ -13,4 +16,24 @@ public:
   virtual ~SmsMessageRecord();
 
 
+  // TODO OS SPECIFIC
+  SmsMessageRecord(/*Context*/ void* context, long id,
+    Body* body, Recipients* recipients,
+    Recipient* individualRecipient,
+    int recipientDeviceId,
+    long dateSent, long dateReceived,
+    int receiptCount,
+    long type, long threadId,
+    int status, /*List<IdentityKeyMismatch> TODO */void* mismatches);
+
+  long GetType() { return this->type; }
+
+  //@Override
+  /*SpannableString*/ unsigned char* GetDisplayBody();
+  //@Override
+  bool IsMms() { return false; }
+  //@Override
+  bool IsMmsNotification() { return false; }
+
+  static int GetGenericDeliveryStatus(int status);
 };
