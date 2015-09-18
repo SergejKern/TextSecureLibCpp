8cc4652b87e9e383f8b4bf15d0b89c3cdb7aa250
Sergej Kern <sk@roboterwerke.eu>
2015-09-18 16:42:43 +0200

    Create MaterialColor.h

---
+++ securesms/color/MaterialColor.h
@@ -0,0 +1,14 @@
+#pragma once
+
+/*
+Port of enum MaterialColor from TextSecure-android
+*/
+
+// [ ] done
+// TFS ID: 324
+
+class MaterialColor
+{
+public:
+  static /*const*/ MaterialColor* GREY; // TODO
+};
