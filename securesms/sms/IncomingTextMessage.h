3686a31b4ff92e5fcb724ae1b05e9e792c907c74
Sergej Kern <sk@roboterwerke.eu>
2015-09-18 10:35:39 +0200

    Create IncomingTextMessage.h

---
+++ securesms/sms/IncomingTextMessage.h
@@ -0,0 +1,59 @@
+/*
+Port of IncomingTextMessage.java from TextSecure-android
+*/
+
+#pragma once
+#include "TextSecureGroup.h"
+
+class IncomingTextMessage
+{
+private:
+  char* message;
+  char* sender;
+  int senderDeviceId;
+  int protocol;
+  char* serviceCenterAddress;
+  bool replyPathPresent;
+  char* pseudoSubject;
+  long sentTimestampMillis;
+  char* groupId;
+  bool push;
+protected:
+  // Constructor from sender and group-id
+  IncomingTextMessage(char* sender, char* groupId);
+public:
+  // Constructor from SmsMessage
+  // SmsMessage is Android class android.telephony.SmsMessage
+  // IncomingTextMessage(SmsMessage message);
+  // Constructor from sender
+  IncomingTextMessage(char* sender, int senderDeviceId, long sentTimestampMillis, char* encodedBody, TextSecureGroup* group);
+  // Parcel in TIZEN?
+  // public IncomingTextMessage(Parcel in);
+  // Copy constructor
+  IncomingTextMessage(IncomingTextMessage* base, char* newBody);
+  // use c++templates?
+  // public IncomingTextMessage(List<IncomingTextMessage> fragments);
+  virtual ~IncomingTextMessage();
+
+  long GetSentTimestampMillis();
+  char* GetPseudoSubject();
+  char* GetMessageBody();
+  IncomingTextMessage* WithMessageBody(char* message);
+  char* GetSender();
+  int GetSenderDeviceId();
+  int GetProtocol();
+  char* GetServiceCenterAddress();
+  bool IsReplyPathPresent();
+  bool IsSecureMessage();
+  bool IsPreKeyBundle();
+  bool IsEndSession();
+  bool IsPush();
+  char* GetGroupId();
+  bool IsGroup();
+
+  //@Override
+  int DescribeContents();
+
+  //@Override
+  //void writeToParcel(Parcel out, int flags);
+};
