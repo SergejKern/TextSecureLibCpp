1259cc49750149048500afa954d28984630374d7
Sergej Kern <sk@roboterwerke.eu>
2015-09-18 12:14:16 +0200

    Update dummymainfortravis.cpp

--- dummymainfortravis.cpp
+++ dummymainfortravis.cpp
@@ -1,3 +1,15 @@
+void * operator new(std::size_t n)
+{
+  void * const p = std::malloc(n);
+  // handle p == 0
+  return p;
+}
+
+void operator delete(void * p) // or delete(void *, std::size_t)
+{
+  std::free(p);
+}
+
 #include "lib/api/messages/TextSecureGroup.h"
 #include "lib/api/messages/TextSecureAttachment.h"
 #include "lib/api/push/TextSecureAddress.h"
