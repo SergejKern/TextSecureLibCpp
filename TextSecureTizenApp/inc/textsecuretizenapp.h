17f059466ffe74d54ea37572a9d6f10f56c1cd77
Sergej Kern <sk@roboterwerke.eu>
2015-09-20 21:58:12 +0200

    Tizen Projekt added

---
+++ TextSecureTizenApp/inc/textsecuretizenapp.h
@@ -0,0 +1,19 @@
+#ifndef __textsecuretizenapp_H__
+#define __textsecuretizenapp_H__
+
+#include <app.h>
+#include <Elementary.h>
+#include <system_settings.h>
+#include <efl_extension.h>
+#include <dlog.h>
+
+#ifdef  LOG_TAG
+#undef  LOG_TAG
+#endif
+#define LOG_TAG "textsecuretizenapp"
+
+#if !defined(PACKAGE)
+#define PACKAGE "org.sk.textsecuretizenapp"
+#endif
+
+#endif /* __textsecuretizenapp_H__ */
