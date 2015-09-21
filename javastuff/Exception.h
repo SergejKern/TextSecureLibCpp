7708772861b8e872c62902232d236df284556b8c
Sergej Kern <sk@roboterwerke.eu>
2015-09-21 17:12:04 +0200

    Create Exception.h

---
+++ javastuff/Exception.h
@@ -0,0 +1,12 @@
+#pragma once
+
+// java.util.concurrent.ExecutionException
+// http://grepcode.com/file/repository.grepcode.com/java/root/jdk/openjdk/6-b27/java/lang/Exception.java#Exception
+
+// [ ] done
+// TFS ID: none
+
+class Exception
+{
+
+};
