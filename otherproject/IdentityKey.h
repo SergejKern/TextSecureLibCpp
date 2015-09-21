44f71ced812be212e3e799c48985585ab6d2afc5
Sergej Kern <sk@roboterwerke.eu>
2015-09-21 16:29:22 +0200

    Create IdentityKey.h

---
+++ otherproject/IdentityKey.h
@@ -0,0 +1,12 @@
+#pragma once
+/*
+port of java class JobManager from WhisperSystems/jobmanager https://github.com/WhisperSystems/jobmanager
+*/
+
+// [ ] done
+// TFS ID: none
+
+class IdentityKey
+{
+
+};
