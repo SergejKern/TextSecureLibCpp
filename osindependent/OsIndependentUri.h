03c4a9ad1845e4cd7303cdcf4c9f7d17919ce9e2
Sergej Kern <sk@roboterwerke.eu>
2015-09-21 16:35:48 +0200

    Create OsIndependentUri.h

---
+++ osindependent/OsIndependentUri.h
@@ -0,0 +1,21 @@
+#pragma once
+/*
+  abstract class OsIndependentCursor to replace android.net.Uri
+  http://developer.android.com/reference/android/net/Uri.html
+*/
+
+// [ ] done
+// TFS ID: 327
+
+class OsIndependentUri
+{
+private:
+public:
+  OsIndependentUri() {};
+  /*
+  Move the cursor to the next row.
+  This method will return false if the cursor is already past the last entry in the result set.
+  http://developer.android.com/reference/android/net/Uri.html
+  */
+  
+};
