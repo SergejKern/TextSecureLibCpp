f14b243b12e22cba1887380171b8cff94d8f2588
Sergej Kern <sk@roboterwerke.eu>
2015-09-21 16:33:55 +0200

    Create OsIndependentSQLiteOpenHelper.h

---
+++ osindependent/OsIndependentSQLiteOpenHelper.h
@@ -0,0 +1,21 @@
+#pragma once
+/*
+  abstract class OsIndependentCursor to replace android.content.Context
+  http://developer.android.com/reference/android/content/Context.html
+*/
+
+// [ ] done
+// TFS ID: 658
+
+class OsIndependentSQLiteOpenHelper
+{
+private:
+public:
+  OsIndependentSQLiteOpenHelper() {};
+  /*
+  Move the cursor to the next row.
+  This method will return false if the cursor is already past the last entry in the result set.
+  http://developer.android.com/reference/android/database/Cursor.html#moveToNext%28%29
+  */
+  
+};
