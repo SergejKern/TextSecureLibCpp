5d86359712cc76f5d9fac16a7a910d1a62198898
Sergej Kern <sk@roboterwerke.eu>
2015-09-21 16:47:39 +0200

    Update MessagingDatabase.h

--- securesms/database/MessagingDatabase.h
+++ securesms/database/MessagingDatabase.h
@@ -9,26 +9,31 @@ Port of class MessagingDatabase from TextSecure-android
 #include "Database.h"
 #include "MmsSmsColumns.h"
 #include "documents\Document.h"
+#include "..\..\osindependent\OsIndependentContext.h"
+#include "..\..\osindependent\OsIndependentSQLiteOpenHelper.h"
+#include "..\..\osindependent\OsIndependentSQLiteDatabase.h"
+#include "..\..\otherproject\IdentityKey.h"
 
 class MessagingDatabase :
-  public Database, MmsSmsColumns
+  public Database, public MmsSmsColumns
 {
 private:
-  static const unsigned char* TAG; // TODO = MessagingDatabase.class.getSimpleName();
+  static const unsigned char* TAG;
 public:
   virtual ~MessagingDatabase();
 
-  MessagingDatabase(/* TODO OS SPECIFIC Context*/ void* context, /* TODO OS SPECIFIC SQLiteOpenHelper*/ void* databaseHelper);
+  MessagingDatabase(OsIndependentContext* context, OsIndependentSQLiteOpenHelper* databaseHelper);
   virtual unsigned char* GetTableName() = 0;
-  void SetMismatchedIdentity(long messageId, const long recipientId, const /* TODO other lib IdentityKey*/ void* identityKey);
-  void AddMismatchedIdentity(long messageId, long recipientId, /* TODO other lib IdentityKey*/ void* identityKey);
-  void RemoveMismatchedIdentity(long messageId, long recipientId, /* TODO other lib IdentityKey*/ void* identityKey);
+  void SetMismatchedIdentity(long messageId, const long recipientId, IdentityKey* identityKey);
+  void AddMismatchedIdentity(long messageId, long recipientId, IdentityKey* identityKey);
+  void RemoveMismatchedIdentity(long messageId, long recipientId, IdentityKey* identityKey);
   /* BIG TODO <D extends Document<I>, I> void RemoveFromDocument(long messageId, String column, I object, Class<D> clazz); */
   /* BIG TODO <T extends Document<I>, I> void addToDocument(long messageId, String column, final I object, Class<T> clazz) throws IOException */
   /* BIG TODO <T extends Document<I>, I> void addToDocument(long messageId, String column, List<I> objects, Class<T> clazz) throws IOException */
 
 private:
-  void SetDocument(/* TODO OS SPECIFIC SQLiteDatabase*/ void* database, long messageId, unsigned char* column, Document* document) /*throws IOException*/;
+  template<typename T> void SetDocument(OsIndependentSQLiteDatabase* database, long messageId, unsigned char* column, Document<T>* document) /*throws IOException*/;
   /* BIG TODO <D extends Document> D getDocument(SQLiteDatabase database, long messageId,String column, Class<D> clazz);*/
 };
 
+const unsigned char* TAG = (unsigned char*)"MessagingDatabase";
