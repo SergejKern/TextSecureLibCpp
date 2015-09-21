8f2a391e5924c1f2f11e300199ebf51587d8833d
Sergej Kern <sk@roboterwerke.eu>
2015-09-21 18:55:38 +0200

    Update IncomingTextMessage.h

--- securesms/sms/IncomingTextMessage.h
+++ securesms/sms/IncomingTextMessage.h
@@ -11,11 +11,11 @@ Port of IncomingTextMessage.java from TextSecure-android
 class IncomingTextMessage
 {
 private:
-  char* message;
-  char* sender;
+  unsigned char* message;
+  unsigned char* sender;
   int senderDeviceId;
   int protocol;
-  char* serviceCenterAddress;
+  unsigned char* serviceCenterAddress;
   bool replyPathPresent;
   char* pseudoSubject;
   long sentTimestampMillis;
@@ -23,41 +23,41 @@ private:
   bool push;
 protected:
   // Constructor from sender and group-id
-  IncomingTextMessage(char* sender, char* groupId);
+  IncomingTextMessage(unsigned char* sender, unsigned char* groupId);
 public:
   // Constructor from SmsMessage
   // SmsMessage is Android class android.telephony.SmsMessage
   // TODO OS SPECIFIC IncomingTextMessage(SmsMessage message);
   // Constructor from sender
-  IncomingTextMessage(char* sender, int senderDeviceId, long sentTimestampMillis, char* encodedBody, TextSecureGroup* group);
+  IncomingTextMessage(unsigned char* sender, int senderDeviceId, long sentTimestampMillis, unsigned char* encodedBody, TextSecureGroup* group);
   // Parcel in TIZEN?
   // public IncomingTextMessage(Parcel in);
   // Copy constructor
-  IncomingTextMessage(IncomingTextMessage* base, char* newBody);
+  IncomingTextMessage(IncomingTextMessage* base, unsigned char* newBody);
   // use c++templates?
   // public IncomingTextMessage(List<IncomingTextMessage> fragments);
   virtual ~IncomingTextMessage();
 
   long GetSentTimestampMillis();
-  char* GetPseudoSubject();
-  char* GetMessageBody();
-  IncomingTextMessage* WithMessageBody(char* message);
-  char* GetSender();
+  unsigned char* GetPseudoSubject();
+  unsigned char* GetMessageBody();
+  IncomingTextMessage* WithMessageBody(unsigned char* message);
+  unsigned char* GetSender();
   int GetSenderDeviceId();
   int GetProtocol();
-  char* GetServiceCenterAddress();
+  unsigned char* GetServiceCenterAddress();
   bool IsReplyPathPresent();
   bool IsSecureMessage();
   bool IsPreKeyBundle();
   bool IsEndSession();
   bool IsPush();
-  char* GetGroupId();
+  unsigned char* GetGroupId();
   bool IsGroup();
 
   //@Override
   int DescribeContents();
 
   //@Override
-  //void writeToParcel(Parcel out, int flags);
+  //void writeToParcel(TODO OS SPECIFIC Parcel out, int flags);
 };
 
