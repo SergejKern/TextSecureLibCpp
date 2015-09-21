5366256467e3ea0a36a381e1a69236b224468378
Sergej Kern <sk@roboterwerke.eu>
2015-09-21 17:11:38 +0200

    Create ListenableFuture.h

---
+++ securesms/util/concurrent/ListenableFuture.h
@@ -0,0 +1,22 @@
+#pragma once
+/*
+Port of class GroupUtil from TextSecure-java
+*/
+
+// [x] done
+// TFS ID: 252
+
+#include "..\..\..\javastuff\ExecutionException.h"
+
+template <typename T> class ListenableFuture<T>
+{
+public:
+  void AddListener(Listener<T> listener);
+
+  class Listener<T>
+  {
+  public:
+    virtual void OnSuccess(T result) = 0;
+    virtual void OnFailure(ExecutionException e) = 0;
+  };
+};
