e54ca6576af8699ab99ab84b1e47da0b5db956cf
Sergej Kern <sk@roboterwerke.eu>
2015-09-21 16:51:11 +0200

    Update SmsDatabase.h

--- securesms/database/SmsDatabase.h
+++ securesms/database/SmsDatabase.h
@@ -15,6 +15,7 @@ Port of class SmsDatabase from TextSecure-android
 #include "model\SmsMessageRecord.h"
 #include "..\sms\IncomingTextMessage.h"
 #include "..\sms\OutgoingTextMessage.h"
+#include "..\..\owntemplates\Pair.h"
 
 class SmsDatabase :
   public MessagingDatabase
@@ -24,10 +25,11 @@ public:
   {
   private:
     OsIndependentCursor* cursor;
+    OsIndependentContext* context;
   protected:
     DisplayRecord::Body GetBody(OsIndependentCursor* cursor);
   public:
-    Reader(OsIndependentCursor* cursor);
+    Reader(OsIndependentCursor* cursor, OsIndependentContext* context);
     SmsMessageRecord* GetNext();
     int GetCount();
     SmsMessageRecord* GetCurrent();
@@ -47,7 +49,7 @@ public:
   };
 public:
   virtual ~SmsDatabase();
-  Reader* readerFor(OsIndependentCursor* cursor) { return new Reader(cursor); }
+  Reader* ReaderFor(OsIndependentCursor* cursor, OsIndependentContext* context) { return new Reader(cursor, context); }
 
 private:
   static const unsigned char* TAG;
@@ -95,10 +97,10 @@ public:
   void IncrementDeliveryReceiptCount(unsigned char* address, long timestamp);
   void SetMessagesRead(long threadId);
   void SetAllMessagesRead();
-  /* TODO Pair<Long, Long>*/void* UpdateMessageBodyAndType(long messageId, unsigned char* body, long maskOff, long maskOn);
-  /* TODO Pair<Long, Long>*/void* CopyMessageInbox(long messageId);
-  /* TODO Pair<Long, Long>*/void* InsertMessageInbox(IncomingTextMessage* message, long type);
-  /* TODO Pair<Long, Long>*/void* InsertMessageInbox(IncomingTextMessage* message);
+  Pair<long, long>* UpdateMessageBodyAndType(long messageId, unsigned char* body, long maskOff, long maskOn);
+  Pair<long, long>* CopyMessageInbox(long messageId);
+  Pair<long, long>* InsertMessageInbox(IncomingTextMessage* message, long type);
+  Pair<long, long>* InsertMessageInbox(IncomingTextMessage* message);
   long InsertMessageOutbox(long threadId, OutgoingTextMessage* message, long type, bool forceSms, long date);
   OsIndependentCursor* GetMessages(int skip, int limit);
   OsIndependentCursor* GetOutgoingMessages();
@@ -106,42 +108,42 @@ public:
   OsIndependentCursor* GetEncryptedRogueMessages(Recipient* recipient);
   OsIndependentCursor* GetMessage(long messageId);
   bool DeleteMessage(long messageId);
-  /*package */void DeleteThread(long threadId);
-  /*package*/void DeleteMessagesInThreadBeforeDate(long threadId, long date);
-  /*package*/ void DeleteThreads(/* TODO Set<Long>*/ void* threadIds);
-  /*package */ void DeleteAllThreads();
+  /*package*/ void DeleteThread(long threadId);
+  /*package*/ void DeleteMessagesInThreadBeforeDate(long threadId, long date);
+  /*package*/ void DeleteThreads(Set<long> threadIds);
+  /*package*/ void DeleteAllThreads();
   /*package*/ OsIndependentSQLiteDatabase* BeginTransaction();
   /*package*/ void EndTransaction(OsIndependentSQLiteDatabase* database);
   /*package*/ OsIndependentSQLiteStatement* CreateInsertStatement(OsIndependentSQLiteDatabase* database);
 };
 
-const unsigned char* SmsDatabase::TAG = "SmsDatabase";
+const unsigned char* SmsDatabase::TAG = (unsigned char*)"SmsDatabase";
 
-const unsigned char* SmsDatabase::TABLE_NAME = "sms";
-const unsigned char* SmsDatabase::PERSON = "person";
-const unsigned char* SmsDatabase::DATE_RECEIVED = "date";
-const unsigned char* SmsDatabase::DATE_SENT = "date_sent";
-const unsigned char* SmsDatabase::PROTOCOL = "protocol";
-const unsigned char* SmsDatabase::STATUS = "status";
-const unsigned char* SmsDatabase::TYPE = "type";
-const unsigned char* SmsDatabase::REPLY_PATH_PRESENT = "reply_path_present";
-const unsigned char* SmsDatabase::SUBJECT = "subject";
-const unsigned char* SmsDatabase::SERVICE_CENTER = "service_center";
-const unsigned char* SmsDatabase::CREATE_TABLE = "CREATE TABLE sms (_id integer PRIMARY KEY, thread_id INTEGER, address TEXT, address_device_id INTEGER DEFAULT 1, person INTEGER, date INTEGER, date_sent INTEGER, protocol INTEGER, read INTEGER DEFAULT 0, status INTEGER DEFAULT -1,type INTEGER, reply_path_present INTEGER, delivery_receipt_count INTEGER DEFAULT 0,subject TEXT, body TEXT, mismatched_identities TEXT DEFAULT NULL, service_center TEXT);";
+const unsigned char* SmsDatabase::TABLE_NAME = (unsigned char*)"sms";
+const unsigned char* SmsDatabase::PERSON = (unsigned char*)"person";
+const unsigned char* SmsDatabase::DATE_RECEIVED = (unsigned char*)"date";
+const unsigned char* SmsDatabase::DATE_SENT = (unsigned char*)"date_sent";
+const unsigned char* SmsDatabase::PROTOCOL = (unsigned char*)"protocol";
+const unsigned char* SmsDatabase::STATUS = (unsigned char*)"status";
+const unsigned char* SmsDatabase::TYPE = (unsigned char*)"type";
+const unsigned char* SmsDatabase::REPLY_PATH_PRESENT = (unsigned char*)"reply_path_present";
+const unsigned char* SmsDatabase::SUBJECT = (unsigned char*)"subject";
+const unsigned char* SmsDatabase::SERVICE_CENTER = (unsigned char*)"service_center";
+const unsigned char* SmsDatabase::CREATE_TABLE = (unsigned char*)"CREATE TABLE sms (_id integer PRIMARY KEY, thread_id INTEGER, address TEXT, address_device_id INTEGER DEFAULT 1, person INTEGER, date INTEGER, date_sent INTEGER, protocol INTEGER, read INTEGER DEFAULT 0, status INTEGER DEFAULT -1,type INTEGER, reply_path_present INTEGER, delivery_receipt_count INTEGER DEFAULT 0,subject TEXT, body TEXT, mismatched_identities TEXT DEFAULT NULL, service_center TEXT);";
 const unsigned char* SmsDatabase::CREATE_INDEXS[] = {
-  "CREATE INDEX IF NOT EXISTS sms_thread_id_index ON sms (thread_id);",
-  "CREATE INDEX IF NOT EXISTS sms_read_index ON sms (read);",
-  "CREATE INDEX IF NOT EXISTS sms_read_and_thread_id_index ON sms(read,thread_id);",
-  "CREATE INDEX IF NOT EXISTS sms_type_index ON sms (type);",
-  "CREATE INDEX IF NOT EXISTS sms_date_sent_index ON sms (date_sent);",
-  "CREATE INDEX IF NOT EXISTS sms_thread_date_index ON sms (thread_id, date);"
+  (unsigned char*)"CREATE INDEX IF NOT EXISTS sms_thread_id_index ON sms (thread_id);",
+  (unsigned char*)"CREATE INDEX IF NOT EXISTS sms_read_index ON sms (read);",
+  (unsigned char*)"CREATE INDEX IF NOT EXISTS sms_read_and_thread_id_index ON sms(read,thread_id);",
+  (unsigned char*)"CREATE INDEX IF NOT EXISTS sms_type_index ON sms (type);",
+  (unsigned char*)"CREATE INDEX IF NOT EXISTS sms_date_sent_index ON sms (date_sent);",
+  (unsigned char*)"CREATE INDEX IF NOT EXISTS sms_thread_date_index ON sms (thread_id, date);"
 };
 const unsigned char* SmsDatabase::MESSAGE_PROJECTION[] = 
 {
   MmsSmsColumns::ID, MmsSmsColumns::THREAD_ID, 
   MmsSmsColumns::ADDRESS, MmsSmsColumns::ADDRESS_DEVICE_ID, PERSON, 
-  "date AS date_received", "date_sent AS date_sent", PROTOCOL, 
-  MmsSmsColumns::READ, STATUS, TYPE, REPLY_PATH_PRESENT, SUBJECT, 
+  (unsigned char*)"date AS date_received", (unsigned char*)"date_sent AS date_sent", 
+  PROTOCOL, MmsSmsColumns::READ, STATUS, TYPE, REPLY_PATH_PRESENT, SUBJECT, 
   MmsSmsColumns::BODY, SERVICE_CENTER, MmsSmsColumns::RECEIPT_COUNT, 
   MmsSmsColumns::MISMATCHED_IDENTITIES
 };
