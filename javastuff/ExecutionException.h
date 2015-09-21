522da261cd955692f7e8ce6e0b41ed2cac6b4f4f
Sergej Kern <sk@roboterwerke.eu>
2015-09-21 17:12:15 +0200

    Create ExecutionException.h

---
+++ javastuff/ExecutionException.h
@@ -0,0 +1,14 @@
+#pragma once
+
+// java.util.concurrent.ExecutionException
+// http://grepcode.com/file/repository.grepcode.com/java/root/jdk/openjdk/6-b27/java/util/concurrent/ExecutionException.java
+
+// [ ] done
+// TFS ID: none
+
+#include "Exception.h"
+
+class ExecutionException : Exception
+{
+
+};
