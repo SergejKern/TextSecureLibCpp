3a79d9ea94490bc17144a0c62f29a219ea6a55d9
Sergej Kern <sk@roboterwerke.eu>
2015-09-22 17:51:09 +0200

    Create OsIndependentConnectivityManager.h

---
+++ osindependent/OsIndependentConnectivityManager.h
@@ -0,0 +1,20 @@
+#pragma once
+/*
+  abstract class OsIndependentCursor to replace android.net.ConnectivityManager
+  http://developer.android.com/reference/android/net/Uri.html
+*/
+
+// [ ] done
+// TFS ID: 672
+
+class OsIndependentConnectivityManager
+{
+private:
+public:
+  /*
+  Move the cursor to the next row.
+  This method will return false if the cursor is already past the last entry in the result set.
+  http://developer.android.com/reference/android/net/Uri.html
+  */
+  
+};
