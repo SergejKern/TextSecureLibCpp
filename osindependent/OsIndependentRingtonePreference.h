7455c4f6ac3fdcbe0af2792a6ce8cf349ed3b0b3
Sergej Kern <sk@roboterwerke.eu>
2015-09-22 17:54:41 +0200

    Create OsIndependentRingtonePreference.h

---
+++ osindependent/OsIndependentRingtonePreference.h
@@ -0,0 +1,24 @@
+#pragma once
+/*
+  abstract class OsIndependentAttributeSet to replace android.util.AttributeSet
+  http://grepcode.com/file/repository.grepcode.com/java/ext/com.google.android/android/5.1.1_r1/android/util/AttributeSet.java#AttributeSet
+*/
+
+// [ ] done
+// TFS ID: 668
+
+#include "OsIndependentContext.h"
+#include "OsIndependentAttributeSet.h"
+
+/* public class RingtonePreference extends Preference implements
+PreferenceManager.OnActivityResultListener */
+class OsIndependentRingtonePreference
+{
+private:
+public:
+  /*
+  */
+  OsIndependentRingtonePreference(OsIndependentContext* context, OsIndependentAttributeSet* attrs, int defStyleAttr);
+  OsIndependentRingtonePreference(OsIndependentContext* context, OsIndependentAttributeSet* attrs);
+  OsIndependentRingtonePreference(OsIndependentContext* context);
+};
