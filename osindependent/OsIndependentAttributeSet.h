b1c7434f82060cb7a7a3a8b47259c0dce7023de0
Sergej Kern <sk@roboterwerke.eu>
2015-09-22 17:50:47 +0200

    Create OsIndependentAttributeSet.h

---
+++ osindependent/OsIndependentAttributeSet.h
@@ -0,0 +1,18 @@
+#pragma once
+/*
+  abstract class OsIndependentAttributeSet to replace android.util.AttributeSet
+  http://grepcode.com/file/repository.grepcode.com/java/ext/com.google.android/android/5.1.1_r1/android/util/AttributeSet.java#AttributeSet
+*/
+
+// [ ] done
+// TFS ID: 667
+
+/* public interface AttributeSet { */
+class OsIndependentAttributeSet
+{
+private:
+public:
+  /*
+  */
+  
+};
