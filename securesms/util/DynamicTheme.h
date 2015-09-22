32e0f28f7e6e32457018cb250de90aa65c8c2d96
Sergej Kern <sk@roboterwerke.eu>
2015-09-22 13:30:38 +0200

    Create DynamicTheme.h

---
+++ securesms/util/DynamicTheme.h
@@ -0,0 +1,56 @@
+#pragma once
+
+/*
+Port of DynamicTheme.java from TextSecure-android
+*/
+
+// [x] done
+// [ ] is ready in TextSecure-android
+// TFS ID: 220
+
+#include "..\..\osindependent\OsIndependentActivity.h"
+#include "..\..\osindependent\OsIndependentIntent.h"
+#include "TextSecurePreferences.h"
+
+// import org.thoughtcrime.securesms.R;
+
+class DynamicTheme
+{
+private:
+  int currentTheme;
+private:
+  class OverridePendingTransition
+  {
+  public:
+    static void Invoke(OsIndependentActivity* activity)
+    {
+      activity->OverridePendingTransition(0, 0);
+    }
+  };
+public:
+  virtual void OnCreate(OsIndependentActivity* activity)
+  {
+    currentTheme = getSelectedTheme(activity);
+    activity->SetTheme(currentTheme);
+  }
+  virtual void OnResume(OsIndependentActivity* activity)
+  {
+    if (currentTheme != getSelectedTheme(activity)) {
+      OsIndependentIntent* intent = activity->GetIntent();
+      activity->Finish();
+      OverridePendingTransition::Invoke(activity);
+      activity->StartActivity(intent);
+      OverridePendingTransition::Invoke(activity);
+    }
+  }
+protected:
+  virtual int GetSelectedTheme(OsIndependentActivity* activity)
+  {
+    unsigned char* theme = TextSecurePreferences::GetTheme(activity);
+    if (theme == "dark")
+    {
+      return R.style.TextSecure_DarkTheme;
+    }
+    return R.style.TextSecure_LightTheme;
+  }
+};
