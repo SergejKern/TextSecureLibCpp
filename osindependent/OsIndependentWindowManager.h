f7f8600c4c0f6d37b26b2ac6b8f9b16edf2530df
Sergej Kern <sk@roboterwerke.eu>
2015-09-22 17:54:52 +0200

    Create OsIndependentWindowManager.h

---
+++ osindependent/OsIndependentWindowManager.h
@@ -0,0 +1,20 @@
+#pragma once
+/*
+  abstract class OsIndependentCursor to replace android.view.WindowManager
+  http://developer.android.com/reference/android/net/Uri.html
+*/
+
+// [ ] done
+// TFS ID: 673
+
+class OsIndependentWindowManager
+{
+private:
+public:
+  /*
+  Move the cursor to the next row.
+  This method will return false if the cursor is already past the last entry in the result set.
+  http://developer.android.com/reference/android/net/Uri.html
+  */
+  
+};
