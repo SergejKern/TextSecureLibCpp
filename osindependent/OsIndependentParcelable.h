8155b5bcbffd1534cb59f91517af664e729e63f7
Sergej Kern <sk@roboterwerke.eu>
2015-09-22 11:10:38 +0200

    Create OsIndependentParcelable.h

---
+++ osindependent/OsIndependentParcelable.h
@@ -0,0 +1,29 @@
+#pragma once
+/*
+  abstract class OsIndependentCursor to replace android.os.Parcelable
+  http://grepcode.com/file/repository.grepcode.com/java/ext/com.google.android/android/5.1.1_r1/android/os/Parcelable.java#Parcelable
+*/
+
+// [ ] done
+// TFS ID: 663
+#include "OsIndependentParcel.h"
+
+class OsIndependentParcelable
+{
+private:
+public:
+  OsIndependentParcelable() {};
+  /*
+  Move the cursor to the next row.
+  This method will return false if the cursor is already past the last entry in the result set.
+  http://developer.android.com/reference/android/net/Uri.html
+  */
+  void WriteToParcel(OsIndependentParcel* parcel, int len);
+  
+  template <typename T> class Creator<T>
+  {
+  public:
+    T* CreateFromParcel(OsIndependentParcel* source);
+    T[] NewArray(int size);
+  };
+};
