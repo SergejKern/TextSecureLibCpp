4f00c19bcf5c517889a64429c6e8df81920bb1db
Sergej Kern <sk@roboterwerke.eu>
2015-09-22 17:54:13 +0200

    Update OsIndependentParcelable.h

--- osindependent/OsIndependentParcelable.h
+++ osindependent/OsIndependentParcelable.h
@@ -18,7 +18,7 @@ public:
   This method will return false if the cursor is already past the last entry in the result set.
   http://developer.android.com/reference/android/net/Uri.html
   */
-  void WriteToParcel(OsIndependentParcel* parcel, int len);
+  virtual void WriteToParcel(OsIndependentParcel* parcel, int len) = 0;
   
   template <typename T> class Creator<T>
   {
