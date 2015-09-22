f692b69bbf010d2690dc359757d78cffdf0f52cf
Sergej Kern <sk@roboterwerke.eu>
2015-09-22 17:56:42 +0200

    Create Map.h

---
+++ owntemplates/Map.h
@@ -0,0 +1,19 @@
+#pragma once
+
+// java.util.Map
+// grepcode.com/file/repository.grepcode.com/java/root/jdk/openjdk/8u40-b25/java/util/Map.java#Map
+
+// [ ] done
+// TFS ID: none
+
+/*
+public interface  Map<K,V> {
+*/
+template <typename K, typename V> class Map
+{
+public:
+  // interface  [More ...] Entry<K,V> {
+  template <typename K, typename V> class Entry
+  {
+  };
+};
