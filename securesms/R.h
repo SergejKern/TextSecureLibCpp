b5f93e82295f75d8b6186f6f00eac0f9c475d536
Sergej Kern <sk@roboterwerke.eu>
2015-09-22 17:49:45 +0200

    Create R.h

---
+++ securesms/R.h
@@ -0,0 +1,23 @@
+#pragma once
+
+/*
+some adapter of generated R object from TextSecure-android
+*/
+
+// [ ] done
+// TFS ID: 666
+
+class R
+{
+public:
+  class Style
+  {
+  public:
+    static const int TextSecure_DarkTheme = 0;
+    static const int TextSecure_LightTheme = 1;
+    static const int TextSecure_DarkIntroTheme = 2;
+    static const int TextSecure_LightIntroTheme = 3;
+    static const int TextSecure_DarkNoActionBar = 4;
+    static const int TextSecure_LightNoActionBar = 5;
+  };
+};
