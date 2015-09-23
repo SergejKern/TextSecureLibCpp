b85697431527e4cc72c757f939de61c2c64e8f18
Sergej Kern <sk@roboterwerke.eu>
2015-09-23 17:18:15 +0200

    Update NetworkFailure.h

--- securesms/database/documents/NetworkFailure.h
+++ securesms/database/documents/NetworkFailure.h
@@ -1,8 +1,26 @@
 #pragma once
+
+/*
+Port of NetworkFailureList from TextSecure-android
+*/
+
+// [x] done
+// TFS ID: 315
+
 class NetworkFailure
 {
 public:
-  NetworkFailure();
   virtual ~NetworkFailure();
-};
+private:
+  //@JsonProperty(value = "r")
+  long recipientId;
 
+public:
+  NetworkFailure(long recipientId);
+  NetworkFailure();
+  long GetRecipientId();
+  ////@Override
+  //bool equals(void* other);
+  ////@Override
+  //int hashCode();
+};
