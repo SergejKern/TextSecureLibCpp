4c0afd94c7cad497fa7c6c69ff3a590d0fe4f432
Sergej Kern <sk@roboterwerke.eu>
2015-09-23 16:40:09 +0200

    Update OsIndependentResources.h

--- osindependent/OsIndependentResources.h
+++ osindependent/OsIndependentResources.h
@@ -1,6 +1,6 @@
 #pragma once
 /*
-  abstract class OsIndependentCursor to replace android.graphics.drawable.Drawable
+  abstract class OsIndependentCursor to replace android.content.res.Resources
   http://grepcode.com/file/repository.grepcode.com/java/ext/com.google.android/android/5.1.1_r1/android/graphics/drawable/Drawable.java#Drawable
 */
 
