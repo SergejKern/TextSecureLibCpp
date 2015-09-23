018db27d20742109aa3d5efb9ca6f90bd427203e
Sergej Kern <sk@roboterwerke.eu>
2015-09-23 13:22:15 +0200

    Create OsIndependentDrawable.h

---
+++ osindependent/OsIndependentDrawable.h
@@ -0,0 +1,17 @@
+#pragma once
+/*
+  abstract class OsIndependentCursor to replace android.graphics.drawable.Drawable
+  http://grepcode.com/file/repository.grepcode.com/java/ext/com.google.android/android/5.1.1_r1/android/graphics/drawable/Drawable.java#Drawable
+*/
+
+// [ ] done
+// TFS ID: 674
+
+/*
+public abstract class Drawable {
+*/
+class OsIndependentDrawable
+{
+public:
+public:
+};
