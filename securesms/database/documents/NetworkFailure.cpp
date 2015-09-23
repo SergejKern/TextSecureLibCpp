88f0f4c0565a1b4d99b50b23f514b891cd2c2f6a
Sergej Kern <sk@roboterwerke.eu>
2015-09-23 17:18:03 +0200

    Update NetworkFailure.cpp

--- securesms/database/documents/NetworkFailure.cpp
+++ securesms/database/documents/NetworkFailure.cpp
@@ -1,11 +1,27 @@
 #include "NetworkFailure.h"
 
-
-NetworkFailure::NetworkFailure()
-{
-}
-
-
 NetworkFailure::~NetworkFailure()
 {
 }
+NetworkFailure::NetworkFailure(long recipientId)
+{
+  this->recipientId = recipientId;
+}
+NetworkFailure::NetworkFailure() {}
+long NetworkFailure::GetRecipientId()
+{
+  return this->recipientId;
+}
+////@Override
+//bool NetworkFailure::equals(void* other)
+//{
+//  if (other == null || !(other instanceof NetworkFailure)) return false;
+//
+//  NetworkFailure that = (NetworkFailure)other;
+//  return this.recipientId == that.recipientId;
+//}
+////@Override
+//int NetworkFailure::hashCode()
+//{
+//  return (int)recipientId;
+//}
