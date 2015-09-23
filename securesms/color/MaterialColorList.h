bd25cf7eda0534edc2abe7c738a9df5e31fe0b08
Sergej Kern <sk@roboterwerke.eu>
2015-09-23 12:04:31 +0200

    Update MaterialColorList.h

--- securesms/color/MaterialColorList.h
+++ securesms/color/MaterialColorList.h
@@ -4,61 +4,51 @@
 Port of class MaterialColorList from TextSecure-android
 */
 
-// [ ] done
+// [x] done
 // TFS ID: 656
 
 #include "MaterialColor.h"
+#include "..\..\osindependent\OsIndependentContext.h"
+#include "..\..\owntemplates\List.h"
 
 class MaterialColorList
 {
 private:
-  //TODO const List<MaterialColor> colors;
-  const MaterialColor* colors;
-
+  List<MaterialColor> colors;
 public:
-  MaterialColorList(MaterialColor* colors)
+  virtual ~MaterialColorList() {}
+  MaterialColorList() {}
+  MaterialColorList(List<MaterialColor> colors)
   {
     this->colors = colors;
   }
-  MaterialColorList() {}
-  virtual ~MaterialColorList() {}
-
   MaterialColor* Get(int index)
   {
-    return nullptr;
-    // TODO return colors->Get(index);
+    return this->colors.Get(index);
   }
-
   int Size()
   {
-    return 0;
-    // TODO return colors->Size();
+    return this->colors.Size();
   }
-
-  // TODO OS SPECIFIC
-  MaterialColor* GetByColor(/*Context*/ void* context, int colorValue)
+  MaterialColor* GetByColor(OsIndependentContext* context, int colorValue)
   {
-    /* TODO for (MaterialColor color : colors) {
-      if (color.represents(context, colorValue)) {
-        return color;
+    for (MaterialColor color : colors)
+    {
+      if (color.Represents(context, colorValue))
+      {
+        return &color;
       }
-    }*/
-
+    }
     return nullptr;
   }
-  // TODO OS SPECIFIC
-  int* AsConversationColorArray(/*Context*/ void* context)
+  int* AsConversationColorArray(OsIndependentContext* context)
   {
-    /* TODO
-    int[] results = new int[Size()];
-    int   index = 0;
-
-    for (MaterialColor color : colors) {
-      results[index++] = color.toConversationColor(context);
+    int* results = new int[this->colors.Size()];
+    int index = 0;
+    for (MaterialColor color : colors)
+    {
+      results[index++] = color.ToConversationColor(context);
     }
-
     return results;
-    */
-    nullptr;
   }
 };
