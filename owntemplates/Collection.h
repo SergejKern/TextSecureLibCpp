a8c0165194c5d3d0d5590439aaaf738d25cd2c8f
Sergej Kern <sk@roboterwerke.eu>
2015-09-23 12:05:14 +0200

    Create Collection.h

---
+++ owntemplates/Collection.h
@@ -0,0 +1,16 @@
+#pragma once
+
+// java.util.Set
+
+// [ ] done
+// TFS ID: none
+
+#include "Iterable.h"
+
+/*
+public interface  [More ...] Collection<E> extends Iterable<E>
+*/
+template <typename E> class Collection : public Iterable<E>
+{
+
+};
