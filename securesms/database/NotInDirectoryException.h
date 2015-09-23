d9f6c129cb3d1ba8cd6d8c67261782d6c10cec72
Sergej Kern <sk@roboterwerke.eu>
2015-09-23 16:57:20 +0200

    Create NotInDirectoryException.h

---
+++ securesms/database/NotInDirectoryException.h
@@ -0,0 +1,13 @@
+#pragma once
+/*
+Port of class NotInDirectoryException from TextSecure-android
+*/
+
+// [x] done
+// TFS ID: 479
+
+#include "..\..\javastuff\Exception.h"
+
+class NotInDirectoryException : Exception /*Throwable*/
+{
+};
