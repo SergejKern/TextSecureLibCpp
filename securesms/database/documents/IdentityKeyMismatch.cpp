19f8b3ba9e763082a79e2cf447a13b3a175c8fb3
Sergej Kern <sk@roboterwerke.eu>
2015-09-23 17:26:07 +0200

    Update IdentityKeyMismatch.cpp

--- securesms/database/documents/IdentityKeyMismatch.cpp
+++ securesms/database/documents/IdentityKeyMismatch.cpp
@@ -1,11 +1,34 @@
 #include "IdentityKeyMismatch.h"
 
-
-IdentityKeyMismatch::IdentityKeyMismatch()
-{
-}
-
-
 IdentityKeyMismatch::~IdentityKeyMismatch()
 {
 }
+IdentityKeyMismatch::IdentityKeyMismatch()
+{
+}
+IdentityKeyMismatch::IdentityKeyMismatch(long recipientId, IdentityKey* identityKey)
+{
+  this->recipientId = recipientId;
+  this->identityKey = identityKey;
+}
+long IdentityKeyMismatch::GetRecipientId()
+{
+  return this->recipientId;
+}
+IdentityKey* IdentityKeyMismatch::GetIdentityKey()
+{
+  return this->identityKey;
+}
+////@Override
+//bool IdentityKeyMismatch::equals(Object other) {
+//  if (other == null || !(other instanceof IdentityKeyMismatch)) {
+//    return false;
+//  }
+//  IdentityKeyMismatch that = (IdentityKeyMismatch)other;
+//  return that.recipientId == this.recipientId && that.identityKey.equals(this.identityKey);
+//}
+////@Override
+//int IdentityKeyMismatch::hashCode()
+//{
+//  return (int)recipientId ^ identityKey.hashCode();
+//}
