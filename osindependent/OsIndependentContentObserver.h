4047a1cdbca84bf506fe8beeff69c217e369712b
Sergej Kern <sk@roboterwerke.eu>
2015-09-23 16:41:49 +0200

    Create OsIndependentContentObserver.h

---
+++ osindependent/OsIndependentContentObserver.h
@@ -0,0 +1,16 @@
+#pragma once
+/*
+  abstract class OsIndependentIntent to replace android.content.ContentResolver
+  http://grepcode.com/file/repository.grepcode.com/java/ext/com.google.android/android/5.1.1_r1/android/content/ContentResolver.java#ContentResolver
+*/
+
+// [ ] done
+// TFS ID: 678
+
+/* public class  Intent implements Parcelable, Cloneable { */
+class OsIndependentContentObserver
+{
+private:
+public:
+
+};
