878c1852bbfe42edff72b568ddc600d8de29d8c1
Sergej Kern <sk@roboterwerke.eu>
2015-09-23 16:38:22 +0200

    Update Set.h

--- owntemplates/Set.h
+++ owntemplates/Set.h
@@ -1,13 +1,17 @@
 #pragma once
 
 // java.util.Set
+// http://grepcode.com/file/repository.grepcode.com/java/root/jdk/openjdk/8u40-b25/java/util/Set.java#Set
 
 // [ ] done
 // TFS ID: none
 
-template <typename T> class Set
+#include "Collection.h"
+
+// public interface Set<E> extends Collection<E> {
+template <typename E> class Set : public Collection<E>
 {
 public:
-  bool Remove(T);
+  bool Remove(E);
   bool IsEmpty();
 };
