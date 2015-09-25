1cc69b0b6ec5517cf428e9f252dafb2582e4ab67
Sergej Kern <sk@roboterwerke.eu>
2015-09-25 12:50:40 +0200

    Update TextSecureAttachment.cpp

--- lib/api/messages/TextSecureAttachment.cpp
+++ lib/api/messages/TextSecureAttachment.cpp
@@ -1,11 +1,56 @@
 #include "TextSecureAttachment.h"
 
-
-TextSecureAttachment::TextSecureAttachment()
+TextSecureAttachment::TextSecureAttachment(unsigned char* contentType)
 {
+  this->contentType = contentType;
+}
+TextSecureAttachment::Builder::Builder() {}
+TextSecureAttachment::Builder* TextSecureAttachment::Builder::WithStream(OsIndependentInputStream* inputStream)
+{
+  this->inputStream = inputStream;
+  return this;
+}
+TextSecureAttachment::Builder* TextSecureAttachment::Builder::WithContentType(unsigned char* contentType)
+{
+  this->contentType = contentType;
+  return this;
+}
+TextSecureAttachment::Builder* TextSecureAttachment::Builder::WithLength(long length)
+{
+  this->length = length;
+  return this;
+}
+TextSecureAttachment::Builder* TextSecureAttachment::Builder::WithListener(ProgressListener* listener)
+{
+  this->listener = listener;
+  return this;
+}
+TextSecureAttachmentStream* TextSecureAttachment::Builder::Build()
+{
+  // TODO if (inputStream == nullptr)
+  //  throw new IllegalArgumentException("Must specify stream!");
+  // TODO if (contentType == nullptr)
+  //  throw new IllegalArgumentException("No content type specified!");
+  // TODO if (length == 0)
+  //  throw new IllegalArgumentException("No length specified!");
+
+  return new TextSecureAttachmentStream(inputStream, contentType, length, listener);
+}
+unsigned char* TextSecureAttachment::GetContentType()
+{
+  return (unsigned char*)this->contentType;
+}
+TextSecureAttachmentStream* TextSecureAttachment::AsStream()
+{
+  return (TextSecureAttachmentStream*)this;
 }
 
-
-TextSecureAttachment::~TextSecureAttachment()
+TextSecureAttachmentPointer* TextSecureAttachment::AsPointer()
 {
+  return (TextSecureAttachmentPointer*)this;
+}
+
+TextSecureAttachment::Builder* TextSecureAttachment::NewStreamBuilder()
+{
+  return new TextSecureAttachment::Builder();
 }
