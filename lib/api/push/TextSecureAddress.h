484d9bdcdbaffc97659287d9be24fe975aa52421
Sergej Kern <sk@roboterwerke.eu>
2015-09-25 17:15:33 +0200

    Update TextSecureAddress.h

--- lib/api/push/TextSecureAddress.h
+++ lib/api/push/TextSecureAddress.h
@@ -1,29 +1,28 @@
-/*
-Port of TextSecureAddress from libtextsecure-java
-*/
-/**
-* Copyright (C) 2014 Open Whisper Systems
-*
-* This program is free software: you can redistribute it and/or modify
-* it under the terms of the GNU General Public License as published by
-* the Free Software Foundation, either version 3 of the License, or
-* (at your option) any later version.
-*
-* This program is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-* GNU General Public License for more details.
-*
-* You should have received a copy of the GNU General Public License
-* along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
 #pragma once
+/*
+Port of class SendMessageResponse from TextSecure-android
+*/
+
+// [x] done
+// TFS ID: 286
+
 class TextSecureAddress
 {
 public:
   static const int DEFAULT_DEVICE_ID = 1;
-  TextSecureAddress();
-  virtual ~TextSecureAddress();
+private:
+  const unsigned char* e164number;
+  const unsigned char* relay;
+public:
+  TextSecureAddress(unsigned char* e164number, unsigned char* relay);
+  TextSecureAddress(unsigned char* e164number);
+  const unsigned char* GetNumber() { return this->e164number; }
+  const unsigned char* GetRelay() { return this->relay; }
+//  //@Override
+//  bool equals(Object other);
+//  //@Override
+//  int hashCode();
+//private:
+//  // TODO override operatoe = if needed
+//  bool equals(unsigned char* one, unsigned char* two);
 };
-
-// TODO
