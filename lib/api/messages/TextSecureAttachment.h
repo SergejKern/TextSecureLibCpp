3fc5a5c2ca5a07d27a9a0be60975aa47306e8138
Sergej Kern <sk@roboterwerke.eu>
2015-09-25 12:50:28 +0200

    Update TextSecureAttachment.h

--- lib/api/messages/TextSecureAttachment.h
+++ lib/api/messages/TextSecureAttachment.h
@@ -1,8 +1,64 @@
 #pragma once
+
+/*
+Port of TextSecureAttachment.java from libtextsecure-java
+*/
+
+// [x] done
+// TFS ID: 265
+
+#include "TextSecureAttachmentPointer.h"
+#include "TextSecureAttachmentStream.h"
+
+#include "..\..\..\osindependent\OsIndependentInputStream.h"
+
 class TextSecureAttachment
 {
-public:
-  TextSecureAttachment();
-  virtual ~TextSecureAttachment();
-};
+private:
+  const unsigned char* contentType;
 
+protected:
+  TextSecureAttachment(unsigned char* contentType)
+  {
+    this->contentType = contentType;
+  }
+public:
+  /**
+  * An interface to receive progress information on upload/download of
+  * an attachment.
+  */
+  class ProgressListener
+  {
+    /**
+    * Called on a progress change event.
+    *
+    * @param total The total amount to transmit/receive in bytes.
+    * @param progress The amount that has been transmitted/received in bytes thus far
+    */
+  public:
+    virtual void OnAttachmentProgress(long total, long progress) = 0;
+  };
+  class Builder
+  {
+  private:
+    OsIndependentInputStream* inputStream;
+    unsigned char* contentType;
+    long length;
+    ProgressListener* listener;
+  public:
+    Builder();
+    Builder* WithStream(OsIndependentInputStream* inputStream);
+    Builder* WithContentType(unsigned char* contentType);
+    Builder* WithLength(long length);
+    Builder* WithListener(ProgressListener* listener);
+  public:
+    TextSecureAttachmentStream* Build();
+  };
+public:
+  unsigned char* GetContentType();
+  virtual bool IsStream() = 0;
+  virtual bool IsPointer() = 0;
+  TextSecureAttachmentStream* AsStream();
+  TextSecureAttachmentPointer* AsPointer();
+  static Builder* NewStreamBuilder();
+};
