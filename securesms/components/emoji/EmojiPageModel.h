a463fa950e79b3ab758091c70d10017eb3523ec6
Sergej Kern <sk@roboterwerke.eu>
2015-09-28 12:59:17 +0200

    Create EmojiPageModel.h

---
+++ securesms/components/emoji/EmojiPageModel.h
@@ -0,0 +1,17 @@
+#pragma once
+/*
+Port of class EmojiPageModel from TextSecure-android
+*/
+
+// [x] done
+// TFS ID: 409
+
+//interface EmojiPageModel
+class EmojiPageModel
+{
+  virtual int GetIconAttr() = 0;
+  virtual unsigned char** GetEmoji() = 0;
+  virtual bool HasSpriteMap() = 0;
+  virtual unsigned char* GetSprite() = 0;
+  virtual bool IsDynamic() = 0;
+};
