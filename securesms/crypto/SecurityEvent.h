66d7b11a340529f3603e37a1523957a4dc02b2dd
Sergej Kern <sk@roboterwerke.eu>
2015-09-28 16:48:09 +0200

    Create SecurityEvent.h

---
+++ securesms/crypto/SecurityEvent.h
@@ -0,0 +1,39 @@
+#pragma once
+
+/*
+Port of SecurityEvent from TextSecure-android
+*/
+
+// [ ] done
+// TFS ID: 227
+
+#include "..\..\osindependent\OsIndependentString.h"
+#include "..\..\osindependent\OsIndependentContext.h"
+#include "..\..\osindependent\OsIndependentIntent.h"
+
+#include "..\service\KeyCachingService.h"
+
+/**
+ * This class processes key exchange interactions.
+ *
+ * @author Moxie Marlinspike
+ */
+class SecurityEvent
+{
+public:
+  static const OsIndependentString* SECURITY_UPDATE_EVENT;
+  static void BroadcastSecurityUpdateEvent(OsIndependentContext* context)
+  {
+    BroadcastSecurityUpdateEvent(context, -2);
+  }
+  static void BroadcastSecurityUpdateEvent(OsIndependentContext* context, long threadId)
+  {
+    //                           TODO : OS SPECIFIC FACTORY
+    OsIndependentIntent* intent = new OsIndependentIntent(SECURITY_UPDATE_EVENT);
+    intent->PutExtra(new OsIndependentString("thread_id"), threadId);
+    intent->SetPackage(context->GetPackageName());
+    context->SendBroadcast(intent, (OsIndependentString*)KeyCachingService::KEY_PERMISSION);
+  }
+};
+
+const OsIndependentString* SecurityEvent::SECURITY_UPDATE_EVENT = new OsIndependentString("org.thoughtcrime.securesms.KEY_EXCHANGE_UPDATE");
