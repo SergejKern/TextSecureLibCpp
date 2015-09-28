fd1c9239615b3b061ea3ca9a0d400b20947a575f
Sergej Kern <sk@roboterwerke.eu>
2015-09-28 13:08:03 +0200

    Update EmojiPageModel.h

--- securesms/components/emoji/EmojiPageModel.h
+++ securesms/components/emoji/EmojiPageModel.h
@@ -10,8 +10,8 @@ Port of class EmojiPageModel from TextSecure-android
 class EmojiPageModel
 {
   virtual int GetIconAttr() = 0;
-  virtual unsigned char** GetEmoji() = 0;
+  virtual const unsigned char** GetEmoji() = 0;
   virtual bool HasSpriteMap() = 0;
-  virtual unsigned char* GetSprite() = 0;
+  virtual const unsigned char* GetSprite() = 0;
   virtual bool IsDynamic() = 0;
 };
