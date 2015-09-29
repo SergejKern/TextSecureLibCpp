acac053527e665b35f89dd63352a8afddc45e0fc
Sergej Kern <sk@roboterwerke.eu>
2015-09-29 12:02:37 +0200

    Create ConversationLoader.h

---
+++ securesms/database/loaders/ConversationLoader.h
@@ -0,0 +1,29 @@
+#pragma once
+/*
+Port of class ConversationLoader from TextSecure-android
+*/
+
+// [x] done
+// TFS ID: 319
+
+#include "..\..\..\osindependent\OsIndependentContext.h"
+#include "..\..\..\osindependent\OsIndependentCursor.h"
+#include "..\DatabaseFactory.h"
+#include "..\..\util\AbstractCursorLoader.h"
+
+class ConversationLoader : AbstractCursorLoader
+{
+private:
+  const long threadId;
+public:
+  ConversationLoader(OsIndependentContext* context, long threadId)
+    : AbstractCursorLoader(context)
+  {
+    this->threadId = threadId;
+  }
+  //@Override
+  OsIndependentCursor* GetCursor()
+  {
+    return DatabaseFactory::GetMmsSmsDatabase(context)->GetConversation(threadId);
+  }
+};
