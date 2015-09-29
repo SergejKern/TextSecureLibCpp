be4a9ed2ce2f69fe9fd4a99a322629e7ba3139ff
Sergej Kern <sk@roboterwerke.eu>
2015-09-29 11:48:11 +0200

    Update CredentialsProvider.h

--- lib/api/util/CredentialsProvider.h
+++ lib/api/util/CredentialsProvider.h
@@ -6,11 +6,13 @@ Port of class CredentialsProvider from TextSecure-android
 // [x] done
 // TFS ID: 297
 
+#include "..\..\..\osindependent\OsIndependentString.h"
+
 //interface CredentialsProvider
 class CredentialsProvider
 {
 public:
-  virtual unsigned char* GetUser() = 0;
-  virtual unsigned char* GetPassword() = 0;
-  virtual unsigned char* GetSignalingKey() = 0;
+  virtual OsIndependentString* GetUser() = 0;
+  virtual OsIndependentString* GetPassword() = 0;
+  virtual OsIndependentString* GetSignalingKey() = 0;
 };
