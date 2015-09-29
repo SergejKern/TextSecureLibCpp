55cf6a94ec0b7460c3ec8b916fdd2ae0b4834092
Sergej Kern <sk@roboterwerke.eu>
2015-09-29 17:44:04 +0200

    Create AccountVerificationTimeoutException.h

---
+++ securesms/service/AccountVerificationTimeoutException.h
@@ -0,0 +1,27 @@
+#pragma once
+/*
+Port of class AccountVerificationTimeoutException from TextSecure-android
+*/
+
+// [ ] done
+// TFS ID: 637
+
+#include "..\..\javastuff\Exception.h"
+#include "..\..\osindependent\OsIndependentString.h"
+
+class AccountVerificationTimeoutException : Exception
+{
+public:
+  AccountVerificationTimeoutException()
+    : Exception()
+  { }
+  AccountVerificationTimeoutException(OsIndependentString* detailMessage)
+    : Exception(detailMessage)
+  { }
+  AccountVerificationTimeoutException(OsIndependentString* detailMessage, Throwable* throwable)
+    : Exception(detailMessage, throwable)
+  { }
+  AccountVerificationTimeoutException(Throwable* throwable)
+    : Exception(throwable)
+  { }
+};
