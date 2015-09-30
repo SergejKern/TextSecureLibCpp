483bdd680b222e8012e6a502c270b5f5a0c5a20e
Sergej Kern <sk@roboterwerke.eu>
2015-09-30 12:49:47 +0200

    Update R.h

--- securesms/R.h
+++ securesms/R.h
@@ -20,4 +20,26 @@ public:
     static const int TextSecure_DarkNoActionBar = 4;
     static const int TextSecure_LightNoActionBar = 5;
   };
+  class Attr
+  {
+  public:
+    static const int emoji_category_people = 0;
+    static const int emoji_category_objects = 1;
+    static const int emoji_category_nature = 2;
+    static const int emoji_category_places = 3;
+    static const int emoji_category_symbol = 4;
+    static const int emoji_category_emoticons = 5;
+  };
+  class String
+  {
+  public:
+    static const int GroupUtil_group_updated = 0;
+    static const int GroupUtil_joined_the_group = 0;
+    static const int GroupUtil_title_is_now = 0;
+  };
+  class Raw
+  {
+  public:
+    static const int whisper = 0;
+  };
 };
