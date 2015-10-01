c46cfba1d74d46a76f545802a2d849e0ed2f9130
Sergej Kern <sk@roboterwerke.eu>
2015-10-01 10:27:11 +0200

    Create OsIndependentContentProviderClient.h

---
+++ osindependent/OsIndependentContentProviderClient.h
@@ -0,0 +1,15 @@
+#pragma once
+/*
+  abstract class OsIndependentContentProviderClient to replace android.content.ContentProviderClient
+  http://grepcode.com/file/repository.grepcode.com/java/ext/com.google.android/android/5.1.1_r1/android/content/ContentProviderClient.java#ContentProviderClient
+*/
+
+// [ ] done
+// TFS ID: 685
+
+// public class ContentProviderClient {
+class OsIndependentContentProviderClient
+{
+private:
+public:
+};
