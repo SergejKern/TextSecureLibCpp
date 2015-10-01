e45f9583ab62115deefde6f82da452290222c6cb
Sergej Kern <sk@roboterwerke.eu>
2015-10-01 16:17:54 +0200

    Create OsIndependentPowerManager.h

---
+++ osindependent/OsIndependentPowerManager.h
@@ -0,0 +1,21 @@
+#pragma once
+/*
+  abstract class OsIndependentCursor to replace android.os.PowerManager
+  http://grepcode.com/file/repository.grepcode.com/java/ext/com.google.android/android/5.1.1_r1/android/os/PowerManager.java#PowerManager
+*/
+
+// [ ] done
+// TFS ID: 765
+
+/*
+public final class  PowerManager {
+*/
+class OsIndependentPowerManager
+{
+public:
+  class WakeLock
+  {
+  public:
+  };
+public:
+};
