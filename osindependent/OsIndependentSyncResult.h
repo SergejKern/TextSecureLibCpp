1f664606af50da11850773fcb8791210638590da
Sergej Kern <sk@roboterwerke.eu>
2015-10-01 10:29:20 +0200

    Create OsIndependentSyncResult.h

---
+++ osindependent/OsIndependentSyncResult.h
@@ -0,0 +1,17 @@
+#pragma once
+/*
+  abstract class OsIndependentSyncResult to replace android.content.SyncResult
+  http://grepcode.com/file/repository.grepcode.com/java/ext/com.google.android/android/5.1.1_r1/android/content/SyncResult.java#SyncResult
+*/
+
+// [ ] done
+// TFS ID: 686
+
+#include "OsIndependentParcelable.h"
+
+// public class Account implements Parcelable {
+class OsIndependentSyncResult : OsIndependentParcelable
+{
+private:
+public:
+};
