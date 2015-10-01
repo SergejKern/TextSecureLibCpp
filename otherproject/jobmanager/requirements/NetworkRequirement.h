53140aee00cc9d9eb1329caec8943f55bccdc674
Sergej Kern <sk@roboterwerke.eu>
2015-10-01 17:09:09 +0200

    Create NetworkRequirement.h

---
+++ otherproject/jobmanager/requirements/NetworkRequirement.h
@@ -0,0 +1,44 @@
+#pragma once
+/*
+Port of class NetworkRequirement from jobmanager-android
+*/
+
+// [x] done
+// TFS ID: 767
+
+#include "..\..\..\osindependent\OsIndependentContext.h"
+#include "..\..\..\osindependent\OsIndependentConnectivityManager.h"
+#include "..\..\..\osindependent\OsIndependentNetworkInfo.h"
+//
+//import org.whispersystems.jobqueue.dependencies.ContextDependent;
+
+#include "Requirement.h"
+#include "..\dependencies\ContextDependent.h"
+
+/**
+ * A requirement that is satisfied when a network connection is present.
+ */
+class NetworkRequirement : public Requirement, public ContextDependent
+{
+private:
+  /*transient */OsIndependentContext* context;
+
+public:
+  NetworkRequirement(OsIndependentContext* context)
+  {
+    this->context = context;
+  }
+  NetworkRequirement() {}
+  //@Override Requirement
+  virtual bool IsPresent() override
+  {
+    OsIndependentConnectivityManager* cm = (OsIndependentConnectivityManager*)context->GetSystemService(OsIndependentContext::CONNECTIVITY_SERVICE);
+    OsIndependentNetworkInfo* netInfo = cm->GetActiveNetworkInfo();
+    return netInfo != nullptr && netInfo->IsConnected();
+  }
+  //@Override ContextDependent
+  void SetContext(OsIndependentContext* context)
+  {
+    this->context = context;
+  }
+};
