1855bf258fef045585691728f9f3ba5177b3ee8d
Sergej Kern <sk@roboterwerke.eu>
2015-10-01 16:49:40 +0200

    Create Serializable.h

---
+++ javastuff/Serializable.h
@@ -0,0 +1,17 @@
+#pragma once
+
+/* 
+   java.io.Serializable
+   http://grepcode.com/file/repo1.maven.org/maven2/org.robolectric/android-all/5.0.0_r2-robolectric-1/java/io/Serializable.java#Serializable
+ */
+
+// [x] done
+// TFS ID: none
+
+#include "Exception.h"
+
+//public interface[More ...] Serializable{
+//}
+
+class Serializable
+{};
