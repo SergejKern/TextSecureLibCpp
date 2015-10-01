b76906adfa192cd6125254265f949b61e580ac52
Sergej Kern <sk@roboterwerke.eu>
2015-10-01 10:25:41 +0200

    Create OsIndependentAccount.h

---
+++ osindependent/OsIndependentAccount.h
@@ -0,0 +1,17 @@
+#pragma once
+/*
+  abstract class OsIndependentCursor to replace android.accounts.Account
+  http://grepcode.com/file/repository.grepcode.com/java/ext/com.google.android/android/5.1.1_r1/android/accounts/Account.java#Account
+*/
+
+// [ ] done
+// TFS ID: 683
+
+#include "OsIndependentParcelable.h"
+
+// public class Account implements Parcelable {
+class OsIndependentAccount : OsIndependentParcelable
+{
+private:
+public:
+};
