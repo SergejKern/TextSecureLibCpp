601b2e46f69940fdca27a05107c27059de153552
Sergej Kern <sk@roboterwerke.eu>
2015-10-01 11:30:31 +0200

    Create OsIndependentFragmentManager.h

---
+++ osindependent/OsIndependentFragmentManager.h
@@ -0,0 +1,17 @@
+#pragma once
+/*
+  abstract class OsIndependentFragmentManager to replace android.support.v4.app.FragmentActivity
+  http://grepcode.com/file/repository.grepcode.com/java/ext/com.google.android/android/5.1.1_r1/android/support/v4/app/FragmentManager.java#FragmentManager
+*/
+
+// [ ] done
+// TFS ID: 758
+
+/*
+public class  [More ...] FragmentActivity extends Activity {
+*/
+class OsIndependentFragmentManager
+{
+public:
+public:
+};
