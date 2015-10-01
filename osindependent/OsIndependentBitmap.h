8a1cf01bb8f8a6391e6ed3cd626c8b5d4f2845f2
Sergej Kern <sk@roboterwerke.eu>
2015-10-01 10:29:44 +0200

    Create OsIndependentBitmap.h

---
+++ osindependent/OsIndependentBitmap.h
@@ -0,0 +1,19 @@
+#pragma once
+/*
+  abstract class OsIndependentCursor to replace  android.graphics.Bitmap
+  http://grepcode.com/file/repository.grepcode.com/java/ext/com.google.android/android/5.1.1_r1/android/graphics/Bitmap.java#Bitmap
+*/
+
+// [ ] done
+// TFS ID: 679
+
+#include "OsIndependentParcelable.h"
+
+/*
+public final class Bitmap implements Parcelable {
+*/
+class OsIndependentBitmap : OsIndependentParcelable
+{
+public:
+public:
+};
