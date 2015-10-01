955cd4add246ecb1dedc40a4153bce540514384d
Sergej Kern <sk@roboterwerke.eu>
2015-10-01 10:28:45 +0200

    Update OsIndependentResources.h

--- osindependent/OsIndependentResources.h
+++ osindependent/OsIndependentResources.h
@@ -1,13 +1,14 @@
 #pragma once
 /*
   abstract class OsIndependentCursor to replace android.content.res.Resources
-  http://grepcode.com/file/repository.grepcode.com/java/ext/com.google.android/android/5.1.1_r1/android/graphics/drawable/Drawable.java#Drawable
+  http://grepcode.com/file/repository.grepcode.com/java/ext/com.google.android/android/5.1.1_r1/android/content/res/Resources.java#Resources
 */
 
 // [ ] done
 // TFS ID: 675
 
 #include "OsIndependentDrawable.h"
+#include "OsIndependentInputStream.h"
 
 /*
 public class Resources {
@@ -18,21 +19,14 @@ public:
   // http://grepcode.com/file/repository.grepcode.com/java/ext/com.google.android/android/5.1.1_r1/android/content/res/Resources.java#Resources.Theme
   // public final class  [More ...] Theme {
   class Theme
-  {
-
-  };
+  { };
 public:
   //@Deprecated
   //@Nullable
   // public Drawable getDrawable(int id) throws NotFoundException {
-  OsIndependentDrawable* GetDrawable(int id)
-  {
-    OsIndependentDrawable* d = GetDrawable(id, nullptr);
-    return d;
-  }
+  virtual OsIndependentDrawable* GetDrawable(int id) = 0;
   // @Nullable
   //public Drawable[More ...] getDrawable(int id, @Nullable Theme theme) throws NotFoundException{
-  OsIndependentDrawable* GetDrawable(int id, Theme* theme)
-  {
-  }
+  virtual OsIndependentDrawable* GetDrawable(int id, Theme* theme) = 0;
+  virtual OsIndependentInputStream* OpenRawResource(int id) = 0;
 };
