26f1769e7fdc2afbcce66698f8187995f9837d1f
Sergej Kern <sk@roboterwerke.eu>
2015-10-02 14:04:15 +0200

    Create RandomAccess.h

---
+++ javastuff/RandomAccess.h
@@ -0,0 +1,15 @@
+#pragma once
+
+/*
+java.util.RandomAccess
+http://grepcode.com/file/repository.grepcode.com/java/root/jdk/openjdk/8u40-b25/java/util/RandomAccess.java#RandomAccess
+ */
+
+// [x] done
+// TFS ID: none
+
+//67 public interface[More ...] RandomAccess{
+//68 }
+
+class RandomAccess
+{};
