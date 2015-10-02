91221c3cbe4f71571ebcd1ccec351910a9fa6337
Sergej Kern <sk@roboterwerke.eu>
2015-10-02 14:04:03 +0200

    Create Cloneable.h

---
+++ javastuff/Cloneable.h
@@ -0,0 +1,15 @@
+#pragma once
+
+/* 
+   java.lang.Cloneable
+   http://grepcode.com/file/repository.grepcode.com/java/root/jdk/openjdk/8u40-b25/java/lang/Cloneable.java#Cloneable
+ */
+
+// [x] done
+// TFS ID: none
+
+//public interface[More ...] Cloneable{
+//}
+
+class Cloneable
+{};
