7d140fd23dd884d2d3be6f5f7a33ff318bc9b6a1
Sergej Kern <sk@roboterwerke.eu>
2015-10-06 17:55:20 +0200

    Update OsIndependentSQLiteOpenHelper.h

--- osindependent/OsIndependentSQLiteOpenHelper.h
+++ osindependent/OsIndependentSQLiteOpenHelper.h
@@ -1,21 +1,31 @@
 #pragma once
 /*
   abstract class OsIndependentCursor to replace android.database.sqlite.SQLiteOpenHelper
-  http://developer.android.com/reference/android/content/Context.html
+  http://grepcode.com/file/repository.grepcode.com/java/ext/com.google.android/android/5.1.1_r1/android/database/sqlite/SQLiteOpenHelper.java#SQLiteOpenHelper
 */
 
 // [ ] done
 // TFS ID: 662
 
+#include "..\Factory\Factory.h"
+
 class OsIndependentSQLiteOpenHelper
 {
 private:
 public:
-  OsIndependentSQLiteOpenHelper() {};
+  OsIndependentSQLiteOpenHelper(
+    OsIndependentContext* context, 
+    OsIndependentString* name, 
+    OsIndependentSQLiteDatabase::CursorFactory* factory,
+    int version)
+  { };
   /*
   Move the cursor to the next row.
   This method will return false if the cursor is already past the last entry in the result set.
   http://developer.android.com/reference/android/database/Cursor.html#moveToNext%28%29
   */
-  
+  virtual void OnCreate(OsIndependentSQLiteDatabase* db) = 0;
+  virtual void OnUpgrade(OsIndependentSQLiteDatabase* db, int oldVersion, int newVersion) = 0;
+  virtual OsIndependentSQLiteDatabase* GetWritableDatabase() = 0;
+  virtual OsIndependentSQLiteDatabase* GetReadableDatabase() = 0;
 };
