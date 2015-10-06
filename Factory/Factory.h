52841804b9a2b20ef5d580ad899578d872fd9434
Sergej Kern <sk@roboterwerke.eu>
2015-10-06 17:59:49 +0200

    Update Factory.h

--- Factory/Factory.h
+++ Factory/Factory.h
@@ -6,5 +6,33 @@
 // [ ] done
 // TFS ID: none
 
-#include "FactoryString.h"
+#include "FactoryContentValues.h"
+#include "FactoryContext.h"
+#include "FactoryCursor.h"
 #include "FactoryPowerManager.h"
+#include "FactorySQLiteCursorDriver.h"
+#include "FactorySQLiteDatabase.h"
+#include "FactorySQLiteOpenHelper.h"
+#include "FactorySQLiteQuery.h"
+#include "FactoryString.h"
+#include "FactoryThread.h"
+
+// bad idea
+//template <typename E> class Factory
+//{
+//private:
+//  // a Instance of implemented plattform specific factory
+//  // which has to be set in plattform specific code
+//  static Factory* instance;
+//public:
+//  // has to be called in plattform specific code
+//  static void SetInstance(Factory* plattformSpecific) { Factory::instance = plattformSpecific; }
+//  static Factory* GetInstance()
+//  {
+//    if (Factory::instance == nullptr)
+//      throw;
+//    return Factory::instance;
+//  }
+//  // interface
+//  virtual E* CreateNewObject() = 0;
+//};
