e56d03ce06400257132131f63f1b0a12ee730963
Sergej Kern <sk@roboterwerke.eu>
2015-10-06 17:54:19 +0200

    Update OsIndependentCursor.h

--- osindependent/OsIndependentCursor.h
+++ osindependent/OsIndependentCursor.h
@@ -12,7 +12,6 @@ class OsIndependentCursor
 {
 private:
 public:
-  OsIndependentCursor() {};
   /*
   Move the cursor to the next row.
   This method will return false if the cursor is already past the last entry in the result set.
@@ -35,7 +34,7 @@ public:
   The result and whether this method throws an exception when the column value is null or the column type is not a string type is implementation-defined.
   http://developer.android.com/reference/android/database/Cursor.html#getString%28int%29
   */
-  virtual unsigned char* GetString(int columnIndex) = 0;
+  virtual OsIndependentString* GetString(int columnIndex) = 0;
   /*
   Returns the value of the requested column as an int.
   The result and whether this method throws an exception when the column value is null, the column type is not an integral type, or the integer value is outside the range [Integer.MIN_VALUE, Integer.MAX_VALUE] is implementation-defined.
@@ -46,12 +45,13 @@ public:
   Returns the zero-based index for the given column name, or throws IllegalArgumentException if the column doesn't exist. If you're not sure if a column will exist or not use getColumnIndex(String) and check for -1, which is more efficient than catching the exceptions.
   http://developer.android.com/reference/android/database/Cursor.html#getColumnIndexOrThrow%28java.lang.String%29
   */
-  virtual int GetColumnIndexOrThrow(unsigned char* columnName) = 0;
+  virtual int GetColumnIndexOrThrow(OsIndependentString* columnName) = 0;
   /*
   Returns the zero-based index for the given column name, or -1 if the column doesn't exist. If you expect the column to exist use getColumnIndexOrThrow(String) instead, which will make the error more clear.
   http://developer.android.com/reference/android/database/Cursor.html#getColumnIndex%28java.lang.String%29
   */
-  virtual int GetColumnIndex(unsigned char* columnName) = 0;
+  virtual int GetColumnIndex(OsIndependentString* columnName) = 0;
   // void  [More ...] setNotificationUri(ContentResolver cr, Uri uri);
   virtual void SetNotificationUri(OsIndependentContentResolver* cr, OsIndependentUri* uri) = 0;
+  virtual void Close() = 0;
 };
