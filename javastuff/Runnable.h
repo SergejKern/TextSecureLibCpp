f351e68e7f903033c9c62f678b91c53653a77fc2
Sergej Kern <sk@roboterwerke.eu>
2015-10-06 17:27:14 +0200

    Create Runnable.h

---
+++ javastuff/Runnable.h
@@ -0,0 +1,20 @@
+#pragma once
+
+/* 
+   java.io.Closeable
+   http://grepcode.com/file/repository.grepcode.com/java/root/jdk/openjdk/7u40-b43/java/io/Closeable.java#Closeable
+ */
+
+// [x] done
+// TFS ID: none
+
+//public interface  [More ...] Runnable {
+//}
+class Runnable
+{
+  /* 
+  When an object implementing interface Runnable is used to create a thread, starting the thread causes the object's run method to be called in that separately executing thread.
+  The general contract of the method run is that it may take any action whatsoever.
+  */
+  virtual void Run() = 0;
+};
