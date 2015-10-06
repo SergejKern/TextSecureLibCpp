fe2a0f6ea3afafbb879890d777ef7eb88fd08feb
Sergej Kern <sk@roboterwerke.eu>
2015-10-06 17:50:41 +0200

    Update Collection.h

--- owntemplates/Collection.h
+++ owntemplates/Collection.h
@@ -14,3 +14,14 @@ template <typename E> class Collection : public Iterable<E>
 {
 
 };
+
+/*
+   java.util.AbstractCollection
+   http://grepcode.com/file/repository.grepcode.com/java/root/jdk/openjdk/8u40-b25/java/util/AbstractCollection.java#AbstractCollection
+*/
+
+// public abstract class  [More ...] AbstractCollection<E> implements Collection<E> {
+template <typename E> class AbstractCollection : public Collection<E>
+{
+
+};
