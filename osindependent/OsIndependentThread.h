2031273c0d873a2996435a9151d24a9d2740dde6
Sergej Kern <sk@roboterwerke.eu>
2015-10-06 17:52:39 +0200

    Create OsIndependentThread.h

---
+++ osindependent/OsIndependentThread.h
@@ -0,0 +1,19 @@
+#pragma once
+/*
+  abstract class OsIndependentCursor to replace java.lang.Thread
+  http://developer.android.com/reference/android/net/Uri.html
+  http://grepcode.com/file/repository.grepcode.com/java/ext/com.google.android/android/5.1.1_r1/android/net/Uri.java#Uri
+*/
+
+// [ ] done
+// TFS ID: 784
+
+#include "..\javastuff\Runnable.h"
+
+// class  [More ...] Thread implements Runnable {
+class OsIndependentThread : Runnable
+{
+private:
+public:
+
+};
