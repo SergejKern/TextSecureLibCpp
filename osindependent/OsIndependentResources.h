7e8028c6658e735591b89bf77a46ed7530351d20
Sergej Kern <sk@roboterwerke.eu>
2015-10-07 15:48:24 +0200

    Update OsIndependentResources.h

--- osindependent/OsIndependentResources.h
+++ osindependent/OsIndependentResources.h
@@ -7,8 +7,7 @@
 // [ ] done
 // TFS ID: 675
 
-#include "OsIndependentDrawable.h"
-#include "OsIndependentInputStream.h"
+#include "..\Factory\Factory.h"
 
 /*
 public class Resources {
@@ -29,4 +28,40 @@ public:
   //public Drawable[More ...] getDrawable(int id, @Nullable Theme theme) throws NotFoundException{
   virtual OsIndependentDrawable* GetDrawable(int id, Theme* theme) = 0;
   virtual OsIndependentInputStream* OpenRawResource(int id) = 0;
+};
+/*
+Plattform independend Factory abstract class.
+This class must be implemented for specific plattforms, to create plattform specific String-classes
+
++------------------------+  creates   +-----------------------+
+| OsIndependentResources |<-----------|    FactoryResources   |
++------------------------+            +-----------------------+
+            ^                                    ^                Plattform independent code
+           /|\                                  /|\
+            |                                    |
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+            |                                    |
+            |                                    |                Plattform specific code
+            |                                    |
++------------------------+  creates   +-----------------------+
+|     Tizen-Resources    |<-----------| TizenFactoryResources |
++------------------------+            +-----------------------+
+*/
+class FactoryResources
+{
+private:
+  // a Instance of implemented plattform specific factory
+  // which has to be set in plattform specific code
+  static FactoryResources* instance;
+public:
+  // has to be called in plattform specific code
+  static void SetInstance(FactoryResources* plattformSpecific) { FactoryResources::instance = plattformSpecific; }
+  static FactoryResources* GetInstance()
+  {
+    if (FactoryResources::instance == nullptr)
+      throw;
+    return FactoryResources::instance;
+  }
+  // interface
+  virtual OsIndependentResources* CreateNewResources() = 0;
 };
