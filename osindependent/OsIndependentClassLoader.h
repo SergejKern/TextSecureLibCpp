b411cb87f6662da431091a47b8a64614645ef28d
Sergej Kern <sk@roboterwerke.eu>
2015-10-07 16:38:49 +0200

    Create OsIndependentClassLoader.h

---
+++ osindependent/OsIndependentClassLoader.h
@@ -0,0 +1,19 @@
+#pragma once
+/*
+  abstract class OsIndependentCursor to replace java.lang.Thread
+  http://developer.android.com/reference/android/net/Uri.html
+  http://grepcode.com/file/repository.grepcode.com/java/ext/com.google.android/android/5.1.1_r1/android/net/Uri.java#Uri
+*/
+
+// [ ] done
+// TFS ID: 786
+
+#include "..\Factory\Factory.h"
+
+// public abstract class  [More ...] ClassLoader {
+class OsIndependentClassLoader
+{
+private:
+public:
+
+};
