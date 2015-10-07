d01a3b3b313c16b9d188276321f797187a9e51cb
Sergej Kern <sk@roboterwerke.eu>
2015-10-07 15:17:36 +0200

    Update OsIndependentKeyEvent.h

--- osindependent/OsIndependentKeyEvent.h
+++ osindependent/OsIndependentKeyEvent.h
@@ -7,16 +7,54 @@
 // [ ] done
 // TFS ID: 757
 
-#include "OsIndependentParcelable.h"
+#include "..\Factory\Factory.h"
 
 /*
 public class  [More ...] KeyEvent extends InputEvent implements Parcelable {
 */
 class OsIndependentKeyEvent : public OsIndependentParcelable
 {
+private:
+  OsIndependentKeyEvent();
 public:
   // TODO REFACTOR
   static const int KEYCODE_MENU = 82;
 public:
 
+};
+/*
+Plattform independend Factory abstract class.
+This class must be implemented for specific plattforms, to create plattform specific String-classes
+
++-----------------------+  creates   +----------------------+
+| OsIndependentKeyEvent |<-----------|    FactoryKeyEvent   |
++-----------------------+            +----------------------+
+           ^                                    ^                Plattform independent code
+          /|\                                  /|\
+           |                                    |
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+           |                                    |
+           |                                    |                Plattform specific code
+           |                                    |
++-----------------------+  creates   +----------------------+
+|     Tizen-KeyEvent    |<-----------| TizenFactoryKeyEvent |
++-----------------------+            +----------------------+
+*/
+class FactoryKeyEvent
+{
+private:
+  // a Instance of implemented plattform specific factory
+  // which has to be set in plattform specific code
+  static FactoryKeyEvent* instance;
+public:
+  // has to be called in plattform specific code
+  static void SetInstance(FactoryKeyEvent* plattformSpecific) { FactoryKeyEvent::instance = plattformSpecific; }
+  static FactoryKeyEvent* GetInstance()
+  {
+    if (FactoryKeyEvent::instance == nullptr)
+      throw;
+    return FactoryKeyEvent::instance;
+  }
+  // interface
+  virtual OsIndependentKeyEvent* CreateNewKeyEvent() = 0;
 };
