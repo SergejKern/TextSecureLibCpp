452f38f956aa61ffba00dc7acf28480241645b51
Sergej Kern <sk@roboterwerke.eu>
2015-10-07 15:43:08 +0200

    Update OsIndependentSQLiteClosable.h

--- osindependent/OsIndependentSQLiteClosable.h
+++ osindependent/OsIndependentSQLiteClosable.h
@@ -19,3 +19,39 @@ public:
   virtual void Close() = 0;
   virtual void ReleaseReference() = 0;
 };
+/*
+Plattform independend Factory abstract class.
+This class must be implemented for specific plattforms, to create plattform specific String-classes
+
++-----------------------------+  creates   +----------------------------+
+| OsIndependentSQLiteClosable |<-----------|    FactorySQLiteClosable   |
++-----------------------------+            +----------------------------+
+               ^                                          ^                Plattform independent code
+              /|\                                        /|\
+               |                                          |
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+               |                                          |
+               |                                          |                Plattform specific code
+               |                                          |
++-----------------------------+  creates   +----------------------------+
+|     Tizen-SQLiteClosable    |<-----------| TizenFactorySQLiteClosable |
++-----------------------------+            +----------------------------+
+*/
+class FactorySQLiteClosable
+{
+private:
+  // a Instance of implemented plattform specific factory
+  // which has to be set in plattform specific code
+  static FactorySQLiteClosable* instance;
+public:
+  // has to be called in plattform specific code
+  static void SetInstance(FactorySQLiteClosable* plattformSpecific) { FactorySQLiteClosable::instance = plattformSpecific; }
+  static FactorySQLiteClosable* GetInstance()
+  {
+    if (FactorySQLiteClosable::instance == nullptr)
+      throw;
+    return FactorySQLiteClosable::instance;
+  }
+  // interface
+  virtual OsIndependentSQLiteClosable* CreateNewSQLiteClosable() = 0;
+};
