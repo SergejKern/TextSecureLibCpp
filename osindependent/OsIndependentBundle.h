fe2f4d6d2110391fb8cb4f1de5f749e043a29ba7
Sergej Kern <sk@roboterwerke.eu>
2015-10-07 15:34:36 +0200

    Update OsIndependentBundle.h

--- osindependent/OsIndependentBundle.h
+++ osindependent/OsIndependentBundle.h
@@ -7,11 +7,48 @@
 // [ ] done
 // TFS ID: 684
 
-#include "OsIndependentParcelable.h"
+#include "..\Factory\Factory.h"
 
 // public final class Bundle extends BaseBundle implements Cloneable, Parcelable{
 class OsIndependentBundle : public OsIndependentParcelable
 {
 private:
+  OsIndependentBundle();
 public:
+};
+/*
+Plattform independend Factory abstract class.
+This class must be implemented for specific plattforms, to create plattform specific String-classes
+
++----------------------+  creates   +---------------------+
+| OsIndependentBundle  |<-----------|    FactoryBundle    |
++----------------------+            +---------------------+
+            ^                                  ^                Plattform independent code
+           /|\                                /|\
+            |                                  |
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+            |                                  |
+            |                                  |                Plattform specific code
+            |                                  |
++----------------------+  creates   +---------------------+
+|     Tizen-Bundle     |<-----------| TizenFactoryBundle  |
++----------------------+            +---------------------+
+*/
+class FactoryBundle
+{
+private:
+  // a Instance of implemented plattform specific factory
+  // which has to be set in plattform specific code
+  static FactoryBundle* instance;
+public:
+  // has to be called in plattform specific code
+  static void SetInstance(FactoryBundle* plattformSpecific) { FactoryBundle::instance = plattformSpecific; }
+  static FactoryBundle* GetInstance()
+  {
+    if (FactoryBundle::instance == nullptr)
+      throw;
+    return FactoryBundle::instance;
+  }
+  // interface
+  virtual OsIndependentBundle* CreateNewBundle() = 0;
 };
