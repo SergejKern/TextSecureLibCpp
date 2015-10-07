1b3265d2186d10879630fe4920c7dad0967961c8
Sergej Kern <sk@roboterwerke.eu>
2015-10-07 14:12:36 +0200

    Update Factory.h

--- Factory/Factory.h
+++ Factory/Factory.h
@@ -6,28 +6,61 @@
 // [ ] done
 // TFS ID: none
 
-#include "FactoryContentValues.h"
-#include "FactoryContext.h"
-#include "FactoryCursor.h"
-#include "FactoryPowerManager.h"
-#include "FactorySQLiteCursorDriver.h"
-#include "FactorySQLiteDatabase.h"
-#include "FactorySQLiteOpenHelper.h"
-#include "FactorySQLiteQuery.h"
-#include "FactoryString.h"
-#include "FactoryThread.h"
+#include "..\osindependent\OsIndependentAbstractThreadedSyncAdapter.h"
+#include "..\osindependent\OsIndependentAccount.h"
+#include "..\osindependent\OsIndependentActivity.h"
+#include "..\osindependent\OsIndependentAttributeSet.h"
+#include "..\osindependent\OsIndependentBitmap.h"
+#include "..\osindependent\OsIndependentBundle.h"
+#include "..\osindependent\OsIndependentConnectivityManager.h"
+#include "..\osindependent\OsIndependentContentObserver.h"
+#include "..\osindependent\OsIndependentContentProviderClient.h"
+#include "..\osindependent\OsIndependentContentResolver.h"
+#include "..\osindependent\OsIndependentContentValues.h"
+#include "..\osindependent\OsIndependentContext.h"
+#include "..\osindependent\OsIndependentCursor.h"
+#include "..\osindependent\OsIndependentDrawable.h"
+#include "..\osindependent\OsIndependentFragmentActivity.h"
+#include "..\osindependent\OsIndependentFragmentManager.h"
+#include "..\osindependent\OsIndependentInputMethodManager.h"
+#include "..\osindependent\OsIndependentInputStream.h"
+#include "..\osindependent\OsIndependentIntent.h"
+#include "..\osindependent\OsIndependentKeyEvent.h"
+#include "..\osindependent\OsIndependentNetworkInfo.h"
+#include "..\osindependent\OsIndependentOutputStream.h"
+#include "..\osindependent\OsIndependentPackageInfo.h"
+#include "..\osindependent\OsIndependentPackageManager.h"
+#include "..\osindependent\OsIndependentParcel.h"
+#include "..\osindependent\OsIndependentParcelable.h"
+#include "..\osindependent\OsIndependentPowerManager.h"
+#include "..\osindependent\OsIndependentResources.h"
+#include "..\osindependent\OsIndependentRingtonePreference.h"
+#include "..\osindependent\OsIndependentSQLiteClosable.h"
+#include "..\osindependent\OsIndependentSQLiteCursorDriver.h"
+#include "..\osindependent\OsIndependentSQLiteDatabase.h"
+#include "..\osindependent\OsIndependentSQLiteOpenHelper.h"
+#include "..\osindependent\OsIndependentSQLiteProgram.h"
+#include "..\osindependent\OsIndependentSQLiteQuery.h"
+#include "..\osindependent\OsIndependentSQLiteStatement.h"
+#include "..\osindependent\OsIndependentString.h"
+#include "..\osindependent\OsIndependentStringBuilder.h"
+#include "..\osindependent\OsIndependentSyncResult.h"
+#include "..\osindependent\OsIndependentSystem.h"
+#include "..\osindependent\OsIndependentThread.h"
+#include "..\osindependent\OsIndependentUri.h"
+#include "..\osindependent\OsIndependentWindowManager.h"
 
-// bad idea
+//// bad idea
 //template <typename E> class Factory
 //{
 //private:
 //  // a Instance of implemented plattform specific factory
 //  // which has to be set in plattform specific code
-//  static Factory* instance;
+//  static Factory<E>* instance;
 //public:
 //  // has to be called in plattform specific code
-//  static void SetInstance(Factory* plattformSpecific) { Factory::instance = plattformSpecific; }
-//  static Factory* GetInstance()
+//  static void SetInstance(Factory<E>* plattformSpecific) { Factory::instance = plattformSpecific; }
+//  static Factory<E>* GetInstance()
 //  {
 //    if (Factory::instance == nullptr)
 //      throw;
