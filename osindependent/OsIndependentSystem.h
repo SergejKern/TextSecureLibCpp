18ae19cfb9d20d667bbf4d0e761343fb91017a95
Sergej Kern <sk@roboterwerke.eu>
2015-10-07 14:52:40 +0200

    Update OsIndependentSystem.h

--- osindependent/OsIndependentSystem.h
+++ osindependent/OsIndependentSystem.h
@@ -7,6 +7,8 @@
 // [ ] done
 // TFS ID: 676
 
+#include "..\Factory\Factory.h"
+
 class OsIndependentSystem
 {
 private:
@@ -16,6 +18,42 @@ public:
   This method will return false if the cursor is already past the last entry in the result set.
   http://developer.android.com/reference/android/net/Uri.html
   */
-  static int GetColorTransparent();
-  static long CurrentTimeMillis();
+  virtual int GetColorTransparent() = 0;
+  virtual long CurrentTimeMillis() = 0;
+};
+/*
+Plattform independend Factory abstract class.
+This class must be implemented for specific plattforms, to create plattform specific String-classes
+
++----------------------+  creates   +---------------------+
+| OsIndependentSystem  |<-----------|    FactorySystem    |
++----------------------+            +---------------------+
+           ^                                 ^                Plattform independent code
+          /|\                               /|\
+           |                                 |
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+           |                                 |
+           |                                 |                Plattform specific code
+           |                                 |
++----------------------+  creates   +---------------------+
+|     Tizen-System     |<-----------| TizenFactorySystem  |
++----------------------+            +---------------------+
+*/
+class FactorySystem
+{
+private:
+  // a Instance of implemented plattform specific factory
+  // which has to be set in plattform specific code
+  static FactorySystem* instance;
+public:
+  // has to be called in plattform specific code
+  static void SetInstance(FactorySystem* plattformSpecific) { FactorySystem::instance = plattformSpecific; }
+  static FactorySystem* GetInstance()
+  {
+    if (FactorySystem::instance == nullptr)
+      throw;
+    return FactorySystem::instance;
+  }
+  // interface
+  virtual OsIndependentSystem* CreateNewSystem() = 0;
 };
