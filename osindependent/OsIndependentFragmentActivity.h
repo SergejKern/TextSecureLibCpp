71ee10403da3778efd6d6ad29fa8cae66b2fbe4c
Sergej Kern <sk@roboterwerke.eu>
2015-10-07 15:25:53 +0200

    Update OsIndependentFragmentActivity.h

--- osindependent/OsIndependentFragmentActivity.h
+++ osindependent/OsIndependentFragmentActivity.h
@@ -7,8 +7,7 @@
 // [ ] done
 // TFS ID: 756
 
-#include "OsIndependentActivity.h"
-#include "OsIndependentFragmentManager.h"
+#include "..\Factory\Factory.h"
 
 /*
 public class  [More ...] FragmentActivity extends Activity {
@@ -21,4 +20,40 @@ public:
   virtual bool OnKeyDown(int keyCode, OsIndependentKeyEvent* event) = 0;
   //public FragmentManager[More ...] getSupportFragmentManager() {
   virtual OsIndependentFragmentManager* GetSupportFragmentManager() = 0;
+};
+/*
+Plattform independend Factory abstract class.
+This class must be implemented for specific plattforms, to create plattform specific String-classes
+
++-------------------------------+  creates   +------------------------------+
+| OsIndependentFragmentActivity |<-----------|    FactoryFragmentActivity   |
++-------------------------------+            +------------------------------+
+                ^                                            ^                Plattform independent code
+               /|\                                          /|\
+                |                                            |
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+                |                                            |
+                |                                            |                Plattform specific code
+                |                                            |
++-------------------------------+  creates   +------------------------------+
+|     Tizen-FragmentActivity    |<-----------| TizenFactoryFragmentActivity |
++-------------------------------+            +------------------------------+
+*/
+class FactoryFragmentActivity
+{
+private:
+  // a Instance of implemented plattform specific factory
+  // which has to be set in plattform specific code
+  static FactoryFragmentActivity* instance;
+public:
+  // has to be called in plattform specific code
+  static void SetInstance(FactoryFragmentActivity* plattformSpecific) { FactoryFragmentActivity::instance = plattformSpecific; }
+  static FactoryFragmentActivity* GetInstance()
+  {
+    if (FactoryFragmentActivity::instance == nullptr)
+      throw;
+    return FactoryFragmentActivity::instance;
+  }
+  // interface
+  virtual OsIndependentFragmentActivity* CreateNewFragmentActivity() = 0;
 };
