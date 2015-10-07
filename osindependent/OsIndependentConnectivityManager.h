0c550261621d5ece2d846af973bff01f5c0e776b
Sergej Kern <sk@roboterwerke.eu>
2015-10-07 15:40:14 +0200

    Update OsIndependentConnectivityManager.h

--- osindependent/OsIndependentConnectivityManager.h
+++ osindependent/OsIndependentConnectivityManager.h
@@ -7,11 +7,47 @@
 // [ ] done
 // TFS ID: 672
 
-#include "OsIndependentNetworkInfo.h"
+#include "..\Factory\Factory.h"
 
 class OsIndependentConnectivityManager
 {
 private:
 public:
   virtual OsIndependentNetworkInfo* GetActiveNetworkInfo() = 0;
+};
+/*
+Plattform independend Factory abstract class.
+This class must be implemented for specific plattforms, to create plattform specific String-classes
+
++----------------------------------+  creates   +---------------------------------+
+| OsIndependentConnectivityManager |<-----------|    FactoryConnectivityManager   |
++----------------------------------+            +---------------------------------+
+                ^                                                 ^                Plattform independent code
+               /|\                                               /|\
+                |                                                 | 
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+                |                                                 |
+                |                                                 |                Plattform specific code
+                |                                                 |
++----------------------------------+  creates   +---------------------------------+
+|     Tizen-ConnectivityManager    |<-----------| TizenFactoryConnectivityManager |
++----------------------------------+            +---------------------------------+
+*/
+class FactoryConnectivityManager
+{
+private:
+  // a Instance of implemented plattform specific factory
+  // which has to be set in plattform specific code
+  static FactoryConnectivityManager* instance;
+public:
+  // has to be called in plattform specific code
+  static void SetInstance(FactoryConnectivityManager* plattformSpecific) { FactoryConnectivityManager::instance = plattformSpecific; }
+  static FactoryConnectivityManager* GetInstance()
+  {
+    if (FactoryConnectivityManager::instance == nullptr)
+      throw;
+    return FactoryConnectivityManager::instance;
+  }
+  // interface
+  virtual OsIndependentConnectivityManager* CreateNewConnectivityManager() = 0;
 };
