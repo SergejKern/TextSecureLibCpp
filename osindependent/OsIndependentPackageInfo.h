cd360c4603ed2584c6194118b50863800471e8a5
Sergej Kern <sk@roboterwerke.eu>
2015-10-07 15:45:14 +0200

    Update OsIndependentPackageInfo.h

--- osindependent/OsIndependentPackageInfo.h
+++ osindependent/OsIndependentPackageInfo.h
@@ -7,15 +7,53 @@
 // [ ] done
 // TFS ID: 670
 
-#include "OsIndependentParcelable.h"
+#include "..\Factory\Factory.h"
 
 /* public class  [More ...] PackageInfo implements Parcelable { */
 class OsIndependentPackageInfo : OsIndependentParcelable
 {
 private:
+  OsIndependentPackageInfo();
 public:
   int versionCode;
   /*
   */
   
+};
+
+/*
+Plattform independend Factory abstract class.
+This class must be implemented for specific plattforms, to create plattform specific String-classes
+
++--------------------------+  creates   +-------------------------+
+| OsIndependentPackageInfo |<-----------|    FactoryPackageInfo   |
++--------------------------+            +-------------------------+
+             ^                                       ^                Plattform independent code
+            /|\                                     /|\
+             |                                       |
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+             |                                       |
+             |                                       |                Plattform specific code
+             |                                       |
++--------------------------+  creates   +-------------------------+
+|     Tizen-PackageInfo    |<-----------| TizenFactoryPackageInfo |
++--------------------------+            +-------------------------+
+*/
+class FactoryPackageInfo
+{
+private:
+  // a Instance of implemented plattform specific factory
+  // which has to be set in plattform specific code
+  static FactoryPackageInfo* instance;
+public:
+  // has to be called in plattform specific code
+  static void SetInstance(FactoryPackageInfo* plattformSpecific) { FactoryPackageInfo::instance = plattformSpecific; }
+  static FactoryPackageInfo* GetInstance()
+  {
+    if (FactoryPackageInfo::instance == nullptr)
+      throw;
+    return FactoryPackageInfo::instance;
+  }
+  // interface
+  virtual OsIndependentPackageInfo* CreateNewPackageInfo() = 0;
 };
