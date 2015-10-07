1427fcf8c95c74521d9929ff00012f31952cd34d
Sergej Kern <sk@roboterwerke.eu>
2015-10-07 14:27:05 +0200

    Update OsIndependentSQLiteQuery.h

--- osindependent/OsIndependentSQLiteQuery.h
+++ osindependent/OsIndependentSQLiteQuery.h
@@ -8,7 +8,6 @@
 // TFS ID: 782
 
 #include "..\Factory\Factory.h"
-#include "OsIndependentSQLiteProgram.h"
 
 // public final class  [More ...] SQLiteQuery extends SQLiteProgram {
 class OsIndependentSQLiteQuery : public OsIndependentSQLiteProgram
@@ -16,4 +15,40 @@ class OsIndependentSQLiteQuery : public OsIndependentSQLiteProgram
 private:
 public:
   
+};
+/*
+Plattform independend Factory abstract class.
+This class must be implemented for specific plattforms, to create plattform specific String-classes
+
++--------------------------+  creates   +-------------------------+
+| OsIndependentSQLiteQuery |<-----------|    FactorySQLiteQuery   |
++--------------------------+            +-------------------------+
+             ^                                       ^                Plattform independent code
+            /|\                                     /|\
+             |                                       |
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+             |                                       |
+             |                                       |                Plattform specific code
+             |                                       |
++--------------------------+  creates   +-------------------------+
+|     Tizen-SQLiteQuery    |<-----------| TizenFactorySQLiteQuery |
++--------------------------+            +-------------------------+
+*/
+class FactorySQLiteQuery
+{
+private:
+  // a Instance of implemented plattform specific factory
+  // which has to be set in plattform specific code
+  static FactorySQLiteQuery* instance;
+public:
+  // has to be called in plattform specific code
+  static void SetInstance(FactorySQLiteQuery* plattformSpecific) { FactorySQLiteQuery::instance = plattformSpecific; }
+  static FactorySQLiteQuery* GetInstance()
+  {
+    if (FactorySQLiteQuery::instance == nullptr)
+      throw;
+    return FactorySQLiteQuery::instance;
+  }
+  // interface
+  virtual OsIndependentSQLiteQuery* CreateNewQuery() = 0;
 };
