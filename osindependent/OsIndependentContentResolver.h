1681ae6fd9b16ac2cedf74cbf639e0dd685a63d5
Sergej Kern <sk@roboterwerke.eu>
2015-10-07 15:54:10 +0200

    Update OsIndependentContentResolver.h

--- osindependent/OsIndependentContentResolver.h
+++ osindependent/OsIndependentContentResolver.h
@@ -7,14 +7,49 @@
 // [ ] done
 // TFS ID: 665
 
-#include "OsIndependentUri.h"
-#include "OsIndependentContentObserver.h"
+#include "..\Factory\Factory.h"
 
 /* public class  Intent implements Parcelable, Cloneable { */
-class OsIndependentContentResolver
+class OsIndependentContentResolver : public OsIndependentParcelable, public Cloneable
 {
 private:
 public:
   // public void  [More ...] notifyChange(Uri uri, ContentObserver observer) {
   virtual void NotifyChange(OsIndependentUri* uri, OsIndependentContentObserver* observer) = 0;
+};
+/*
+Plattform independend Factory abstract class.
+This class must be implemented for specific plattforms, to create plattform specific String-classes
+
++------------------------------+  creates   +-----------------------------+
+| OsIndependentContentResolver |<-----------|    FactoryContentResolver   |
++------------------------------+            +-----------------------------+
+              ^                                             ^                Plattform independent code
+             /|\                                           /|\
+              |                                             |
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+              |                                             |
+              |                                             |                Plattform specific code
+              |                                             |
++------------------------------+  creates   +-----------------------------+
+|     Tizen-ContentResolver    |<-----------| TizenFactoryContentResolver |
++------------------------------+            +-----------------------------+
+*/
+class FactoryContentResolver
+{
+private:
+  // a Instance of implemented plattform specific factory
+  // which has to be set in plattform specific code
+  static FactoryContentResolver* instance;
+public:
+  // has to be called in plattform specific code
+  static void SetInstance(FactoryContentResolver* plattformSpecific) { FactoryContentResolver::instance = plattformSpecific; }
+  static FactoryContentResolver* GetInstance()
+  {
+    if (FactoryContentResolver::instance == nullptr)
+      throw;
+    return FactoryContentResolver::instance;
+  }
+  // interface
+  virtual OsIndependentContentResolver* CreateNewContentResolver() = 0;
 };
