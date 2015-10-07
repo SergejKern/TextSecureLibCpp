95a2baaa1e68cd687dd43c3b6ee9c9f6632b1349
Sergej Kern <sk@roboterwerke.eu>
2015-10-07 15:28:46 +0200

    Update OsIndependentDrawable.h

--- osindependent/OsIndependentDrawable.h
+++ osindependent/OsIndependentDrawable.h
@@ -7,11 +7,50 @@
 // [ ] done
 // TFS ID: 674
 
+#include "..\Factory\Factory.h"
+
 /*
 public abstract class Drawable {
 */
 class OsIndependentDrawable
 {
+private:
+  OsIndependentDrawable();
 public:
+};
+/*
+Plattform independend Factory abstract class.
+This class must be implemented for specific plattforms, to create plattform specific String-classes
+
++-----------------------+  creates   +----------------------+
+| OsIndependentDrawable |<-----------|    FactoryDrawable   |
++-----------------------+            +----------------------+
+           ^                                    ^                Plattform independent code
+          /|\                                  /|\
+           |                                    |
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+           |                                    |
+           |                                    |                Plattform specific code
+           |                                    |
++-----------------------+  creates   +----------------------+
+|     Tizen-Drawable    |<-----------| TizenFactoryDrawable |
++-----------------------+            +----------------------+
+*/
+class FactoryDrawable
+{
+private:
+  // a Instance of implemented plattform specific factory
+  // which has to be set in plattform specific code
+  static FactoryDrawable* instance;
 public:
+  // has to be called in plattform specific code
+  static void SetInstance(FactoryDrawable* plattformSpecific) { FactoryDrawable::instance = plattformSpecific; }
+  static FactoryDrawable* GetInstance()
+  {
+    if (FactoryDrawable::instance == nullptr)
+      throw;
+    return FactoryDrawable::instance;
+  }
+  // interface
+  virtual OsIndependentDrawable* CreateNewDrawable() = 0;
 };
