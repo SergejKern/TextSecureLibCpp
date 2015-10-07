7057494a051f7adc9a64cf560a51787366acf787
Sergej Kern <sk@roboterwerke.eu>
2015-10-07 14:33:51 +0200

    Update OsIndependentPowerManager.h

--- osindependent/OsIndependentPowerManager.h
+++ osindependent/OsIndependentPowerManager.h
@@ -7,6 +7,8 @@
 // [ ] done
 // TFS ID: 765
 
+#include "..\Factory\Factory.h"
+
 /*
 public final class  PowerManager {
 */
@@ -25,4 +27,40 @@ public:
   static const int PARTIAL_WAKE_LOCK = 0x00000001;
 public:
   virtual WakeLock* NewWakeLock(int levelAndFlags, OsIndependentString* tag) = 0;
+};
+/*
+Plattform independend Factory abstract class.
+This class must be implemented for specific plattforms, to create plattform specific String-classes
+
++---------------------------+  creates   +--------------------------+
+| OsIndependentPowerManager |<-----------|    FactoryPowerManager   |
++---------------------------+            +--------------------------+
+              ^                                       ^                Plattform independent code
+             /|\                                     /|\
+              |                                       |
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+              |                                       |
+              |                                       |                Plattform specific code
+              |                                       |
++---------------------------+  creates   +--------------------------+
+|     Tizen-PowerManager    |<-----------| TizenFactoryPowerManager |
++---------------------------+            +--------------------------+
+*/
+class FactoryPowerManager
+{
+private:
+  // a Instance of implemented plattform specific factory
+  // which has to be set in plattform specific code
+  static FactoryPowerManager* instance;
+public:
+  // has to be called in plattform specific code
+  static void SetInstance(FactoryPowerManager* plattformSpecific) { FactoryPowerManager::instance = plattformSpecific; }
+  static FactoryPowerManager* GetInstance()
+  {
+    if (FactoryPowerManager::instance == nullptr)
+      throw;
+    return FactoryPowerManager::instance;
+  }
+  // interface
+  virtual OsIndependentPowerManager* CreateNewPowerManager() = 0;
 };
