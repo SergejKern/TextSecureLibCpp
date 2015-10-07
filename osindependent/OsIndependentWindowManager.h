bf986cac713fcd33470c01b15eb85583c3cdf4f9
Sergej Kern <sk@roboterwerke.eu>
2015-10-07 15:14:59 +0200

    Update OsIndependentWindowManager.h

--- osindependent/OsIndependentWindowManager.h
+++ osindependent/OsIndependentWindowManager.h
@@ -1,20 +1,69 @@
 #pragma once
 /*
   abstract class OsIndependentCursor to replace android.view.WindowManager
-  http://developer.android.com/reference/android/net/Uri.html
+  http://grepcode.com/file/repository.grepcode.com/java/ext/com.google.android/android/5.1.1_r1/android/view/WindowManager.java#WindowManager
 */
 
 // [ ] done
 // TFS ID: 673
 
-class OsIndependentWindowManager
+#include "..\Factory\Factory.h"
+
+// done [ ]
+// public interface  [More ...] ViewManager
+// public abstract class  [More ...] ViewGroup extends View implements ViewParent, ViewManager {
+// public class  [More ...] View implements Drawable.Callback, KeyEvent.Callback, AccessibilityEventSource{
+class OsIndependentViewManager
+{
+public:
+  //void[More ...] addView(View view, ViewGroup.LayoutParams params);
+  virtual void AddView() = 0;
+  //void[More ...] updateViewLayout(View view, ViewGroup.LayoutParams params);
+  virtual void UpdateViewLayout() = 0;
+  //void[More ...] removeView(View view);
+  virtual void RemoveView() = 0;
+};
+
+// public interface  [More ...] WindowManager extends ViewManager {
+class OsIndependentWindowManager : public OsIndependentViewManager
 {
 private:
+  OsIndependentWindowManager();
 public:
-  /*
-  Move the cursor to the next row.
-  This method will return false if the cursor is already past the last entry in the result set.
-  http://developer.android.com/reference/android/net/Uri.html
-  */
-  
+};
+/*
+Plattform independend Factory abstract class.
+This class must be implemented for specific plattforms, to create plattform specific String-classes
+
++----------------------------+  creates   +---------------------------+
+| OsIndependentWindowManager |<-----------|    FactoryWindowManager   |
++----------------------------+            +---------------------------+
+               ^                                        ^                Plattform independent code
+              /|\                                      /|\
+               |                                        |
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+               |                                        |
+               |                                        |                Plattform specific code
+               |                                        |
++----------------------------+  creates   +---------------------------+
+|     Tizen-WindowManager    |<-----------| TizenFactoryWindowManager |
++----------------------------+            +---------------------------+
+*/
+class FactoryWindowManager
+{
+private:
+  // a Instance of implemented plattform specific factory
+  // which has to be set in plattform specific code
+  static FactoryWindowManager* instance;
+public:
+  // has to be called in plattform specific code
+  static void SetInstance(FactoryWindowManager* plattformSpecific) { FactoryWindowManager::instance = plattformSpecific; }
+  static FactoryWindowManager* GetInstance()
+  {
+    if (FactoryWindowManager::instance == nullptr)
+      throw;
+    return FactoryWindowManager::instance;
+  }
+  // interface
+  virtual OsIndependentWindowManager* CreateNewWindowManager() = 0;
 };
