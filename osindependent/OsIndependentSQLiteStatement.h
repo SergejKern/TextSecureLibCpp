4e5ef2da45ef14a2e43148c16bc269e7a201a097
Sergej Kern <sk@roboterwerke.eu>
2015-10-07 14:50:43 +0200

    Update OsIndependentSQLiteStatement.h

--- osindependent/OsIndependentSQLiteStatement.h
+++ osindependent/OsIndependentSQLiteStatement.h
@@ -1,21 +1,53 @@
 #pragma once
 /*
   abstract class OsIndependentCursor to replace android.database.sqlite.SQLiteStatement
-  http://developer.android.com/reference/android/content/Context.html
+  http://developer.android.com/reference/android/content/SQLiteStatement.html
 */
 
 // [ ] done
 // TFS ID: 660
 
+#include "..\Factory\Factory.h"
+
 class OsIndependentSQLiteStatement
 {
 private:
+  OsIndependentSQLiteStatement();
+public:  
+};
+/*
+Plattform independend Factory abstract class.
+This class must be implemented for specific plattforms, to create plattform specific String-classes
+
++------------------------------+  creates   +-----------------------------+
+| OsIndependentSQLiteStatement |<-----------|    FactorySQLiteStatement   |
++------------------------------+            +-----------------------------+
+               ^                                           ^                Plattform independent code
+              /|\                                         /|\
+               |                                           |
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+               |                                           |
+               |                                           |                Plattform specific code
+               |                                           |
++------------------------------+  creates   +-----------------------------+
+|     Tizen-SQLiteStatement    |<-----------| TizenFactorySQLiteStatement |
++------------------------------+            +-----------------------------+
+*/
+class FactorySQLiteStatement
+{
+private:
+  // a Instance of implemented plattform specific factory
+  // which has to be set in plattform specific code
+  static FactorySQLiteStatement* instance;
 public:
-  OsIndependentSQLiteStatement() {};
-  /*
-  Move the cursor to the next row.
-  This method will return false if the cursor is already past the last entry in the result set.
-  http://developer.android.com/reference/android/database/Cursor.html#moveToNext%28%29
-  */
-  
+  // has to be called in plattform specific code
+  static void SetInstance(FactorySQLiteStatement* plattformSpecific) { FactorySQLiteStatement::instance = plattformSpecific; }
+  static FactorySQLiteStatement* GetInstance()
+  {
+    if (FactorySQLiteStatement::instance == nullptr)
+      throw;
+    return FactorySQLiteStatement::instance;
+  }
+  // interface
+  virtual OsIndependentSQLiteStatement* CreateNewStatement() = 0;
 };
