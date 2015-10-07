3d4419deeef1c5abb63adc2c5e76a549900c35d5
Sergej Kern <sk@roboterwerke.eu>
2015-10-07 14:38:10 +0200

    Update OsIndependentAccount.h

--- osindependent/OsIndependentAccount.h
+++ osindependent/OsIndependentAccount.h
@@ -7,11 +7,47 @@
 // [ ] done
 // TFS ID: 683
 
-#include "OsIndependentParcelable.h"
+#include "..\Factory\Factory.h"
 
 // public class Account implements Parcelable {
 class OsIndependentAccount : OsIndependentParcelable
 {
 private:
 public:
+};
+/*
+Plattform independend Factory abstract class.
+This class must be implemented for specific plattforms, to create plattform specific String-classes
+
++-----------------------------+  creates   +----------------------------+
+| OsIndependentAccount        |<-----------|    FactoryAccount          |
++-----------------------------+            +----------------------------+
+             ^                                          ^                Plattform independent code
+            /|\                                        /|\
+             |                                          |
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+             |                                          |
+             |                                          |                Plattform specific code
+             |                                          |
++-----------------------------+  creates   +----------------------------+
+|     Tizen-Account           |<-----------| TizenFactoryAccount        |
++-----------------------------+            +----------------------------+
+*/
+class FactoryAccount
+{
+private:
+  // a Instance of implemented plattform specific factory
+  // which has to be set in plattform specific code
+  static FactoryAccount* instance;
+public:
+  // has to be called in plattform specific code
+  static void SetInstance(FactoryAccount* plattformSpecific) { FactoryAccount::instance = plattformSpecific; }
+  static FactoryAccount* GetInstance()
+  {
+    if (FactoryAccount::instance == nullptr)
+      throw;
+    return FactoryAccount::instance;
+  }
+  // interface
+  virtual OsIndependentAccount* CreateNewAccount() = 0;
 };
