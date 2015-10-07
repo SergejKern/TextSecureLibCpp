61ae16778969d63f2557784351a461d93d23bc10
Sergej Kern <sk@roboterwerke.eu>
2015-10-07 16:12:40 +0200

    Update OsIndependentSQLiteProgram.h

--- osindependent/OsIndependentSQLiteProgram.h
+++ osindependent/OsIndependentSQLiteProgram.h
@@ -9,46 +9,14 @@
 
 #include "..\Factory\Factory.h"
 
-// public final class  [More ...] SQLiteQuery extends SQLiteProgram {
+// public abstract class  [More ...] SQLiteProgram extends SQLiteClosable {
 class OsIndependentSQLiteProgram : public OsIndependentSQLiteClosable
 {
 private:
+  OsIndependentSQLiteProgram();
 public:
   
 };
 /*
-Plattform independend Factory abstract class.
-This class must be implemented for specific plattforms, to create plattform specific String-classes
-
-+----------------------------+  creates   +---------------------------+
-| OsIndependentSQLiteProgram |<-----------|    FactorySQLiteProgram   |
-+----------------------------+            +---------------------------+
-              ^                                 ^                Plattform independent code
-             /|\                               /|\
-              |                                 |
-~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-              |                                 |
-              |                                 |                Plattform specific code
-              |                                 |
-+----------------------------+  creates   +---------------------------+
-|     Tizen-SQLiteProgram    |<-----------| TizenFactorySQLiteProgram |
-+----------------------------+            +---------------------------+
+needs no Factory, because is abstract
 */
-class FactorySQLiteProgram
-{
-private:
-  // a Instance of implemented plattform specific factory
-  // which has to be set in plattform specific code
-  static FactorySQLiteProgram* instance;
-public:
-  // has to be called in plattform specific code
-  static void SetInstance(FactorySQLiteProgram* plattformSpecific) { FactorySQLiteProgram::instance = plattformSpecific; }
-  static FactorySQLiteProgram* GetInstance()
-  {
-    if (FactorySQLiteProgram::instance == nullptr)
-      throw;
-    return FactorySQLiteProgram::instance;
-  }
-  // interface
-  virtual OsIndependentSQLiteProgram* CreateNewSQLiteProgram() = 0;
-};
