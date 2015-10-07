c2f4b5c3e2b674b9ce57cd94ded85f1b122eee73
Sergej Kern <sk@roboterwerke.eu>
2015-10-07 15:50:06 +0200

    Update OsIndependentPackageManager.h

--- osindependent/OsIndependentPackageManager.h
+++ osindependent/OsIndependentPackageManager.h
@@ -7,7 +7,7 @@
 // [ ] done
 // TFS ID: 669
 
-#include "OsIndependentPackageInfo.h"
+#include "..\Factory\Factory.h"
 
 class OsIndependentPackageManager
 {
@@ -16,4 +16,40 @@ public:
   /*
   */
   virtual OsIndependentPackageInfo* GetPackageInfo(unsigned char* packageName, int flags) = 0;
+};
+/*
+Plattform independend Factory abstract class.
+This class must be implemented for specific plattforms, to create plattform specific String-classes
+
++-----------------------------+  creates   +----------------------------+
+| OsIndependentPackageManager |<-----------|    FactoryPackageManager   |
++-----------------------------+            +----------------------------+
+              ^                                           ^                Plattform independent code
+             /|\                                         /|\
+              |                                           |
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+              |                                           |
+              |                                           |                Plattform specific code
+              |                                           |
++-----------------------------+  creates   +----------------------------+
+|     Tizen-PackageManager    |<-----------| TizenFactoryPackageManager |
++-----------------------------+            +----------------------------+
+*/
+class FactoryPackageManager
+{
+private:
+  // a Instance of implemented plattform specific factory
+  // which has to be set in plattform specific code
+  static FactoryPackageManager* instance;
+public:
+  // has to be called in plattform specific code
+  static void SetInstance(FactoryPackageManager* plattformSpecific) { FactoryPackageManager::instance = plattformSpecific; }
+  static FactoryPackageManager* GetInstance()
+  {
+    if (FactoryPackageManager::instance == nullptr)
+      throw;
+    return FactoryPackageManager::instance;
+  }
+  // interface
+  virtual OsIndependentPackageManager* CreateNewPackageManager() = 0;
 };
