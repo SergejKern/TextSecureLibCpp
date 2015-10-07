c0463ea9fdf07d57f368efa5a663f438f0245342
Sergej Kern <sk@roboterwerke.eu>
2015-10-07 15:27:10 +0200

    Update OsIndependentBitmap.h

--- osindependent/OsIndependentBitmap.h
+++ osindependent/OsIndependentBitmap.h
@@ -7,7 +7,7 @@
 // [ ] done
 // TFS ID: 679
 
-#include "OsIndependentParcelable.h"
+#include "..\Factory\Factory.h"
 
 /*
 public final class Bitmap implements Parcelable {
@@ -15,5 +15,42 @@ public final class Bitmap implements Parcelable {
 class OsIndependentBitmap : OsIndependentParcelable
 {
 public:
+  OsIndependentBitmap();
 public:
+};
+/*
+Plattform independend Factory abstract class.
+This class must be implemented for specific plattforms, to create plattform specific String-classes
+
++----------------------+  creates   +---------------------+
+| OsIndependentBitmap  |<-----------|    FactoryBitmap    |
++----------------------+            +---------------------+
+            ^                                 ^                Plattform independent code
+           /|\                               /|\
+            |                                 |
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+            |                                 |
+            |                                 |                Plattform specific code
+            |                                 |
++----------------------+  creates   +---------------------+
+|     Tizen-Bitmap     |<-----------| TizenFactoryBitmap  |
++----------------------+            +---------------------+
+*/
+class FactoryBitmap
+{
+private:
+  // a Instance of implemented plattform specific factory
+  // which has to be set in plattform specific code
+  static FactoryBitmap* instance;
+public:
+  // has to be called in plattform specific code
+  static void SetInstance(FactoryBitmap* plattformSpecific) { FactoryBitmap::instance = plattformSpecific; }
+  static FactoryBitmap* GetInstance()
+  {
+    if (FactoryBitmap::instance == nullptr)
+      throw;
+    return FactoryBitmap::instance;
+  }
+  // interface
+  virtual OsIndependentBitmap* CreateNewBitmap() = 0;
 };
