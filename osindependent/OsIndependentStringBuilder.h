d2cf0951463c95e315f45f8af2befc05bb823fd6
Sergej Kern <sk@roboterwerke.eu>
2015-10-07 14:54:57 +0200

    Update OsIndependentStringBuilder.h

--- osindependent/OsIndependentStringBuilder.h
+++ osindependent/OsIndependentStringBuilder.h
@@ -7,7 +7,7 @@
 // [ ] done
 // TFS ID: 688
 
-#include "OsIndependentString.h"
+#include "..\Factory\Factory.h"
 
 /*
 a abstract class to be overvritten for every platform.
@@ -19,9 +19,45 @@ http://doc.qt.io/qt-4.8/qstring.html
 class OsIndependentStringBuilder
 {
 private:
-  int count;
 public:
-  OsIndependentStringBuilder* Append(OsIndependentString* str);
-  /* java.lang.AbstractStringBuilder.length()*/ int Length() { return count; }
-  OsIndependentString* ToString();
+  virtual OsIndependentStringBuilder* Append(OsIndependentString* str) = 0;
+  /* java.lang.AbstractStringBuilder.length()*/ 
+  virtual int Length() = 0;
+  virtual OsIndependentString* ToString() = 0;
+};
+/*
+Plattform independend Factory abstract class.
+This class must be implemented for specific plattforms, to create plattform specific String-classes
+
++----------------------------+  creates   +---------------------------+
+| OsIndependentStringBuilder |<-----------|    FactoryStringBuilder   |
++----------------------------+            +---------------------------+
+              ^                                         ^                Plattform independent code
+             /|\                                       /|\
+              |                                         |
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+              |                                         |
+              |                                         |                Plattform specific code
+              |                                         |
++----------------------------+  creates   +---------------------------+
+|     Tizen-StringBuilder    |<-----------| TizenFactoryStringBuilder |
++----------------------------+            +---------------------------+
+*/
+class FactoryStringBuilder
+{
+private:
+  // a Instance of implemented plattform specific factory
+  // which has to be set in plattform specific code
+  static FactoryStringBuilder* instance;
+public:
+  // has to be called in plattform specific code
+  static void SetInstance(FactoryStringBuilder* plattformSpecific) { FactoryStringBuilder::instance = plattformSpecific; }
+  static FactoryStringBuilder* GetInstance()
+  {
+    if (FactoryStringBuilder::instance == nullptr)
+      throw;
+    return FactoryStringBuilder::instance;
+  }
+  // interface
+  virtual OsIndependentStringBuilder* CreateNewStringBuilder() = 0;
 };
