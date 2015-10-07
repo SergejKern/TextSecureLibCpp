f16f0c649c244fdb1394d78a31e1e1d587161178
Sergej Kern <sk@roboterwerke.eu>
2015-10-07 14:20:37 +0200

    Update OsIndependentContentValues.h

--- osindependent/OsIndependentContentValues.h
+++ osindependent/OsIndependentContentValues.h
@@ -17,3 +17,39 @@ public:
   virtual void Put(OsIndependentString* key, bool value) = 0;
   virtual void Put(OsIndependentString* key, OsIndependentString* value) = 0;
 };
+/*
+Plattform independend Factory abstract class.
+This class must be implemented for specific plattforms, to create plattform specific String-classes
+
++----------------------------+            +---------------------------+
+| OsIndependentContentValues |<-----------|    FactoryContentValues   |
++----------------------------+            +---------------------------+
+              ^                                         ^                Plattform independent code
+             /|\                                       /|\
+              |                                         |
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+              |                                         |
+              |                                         |                Plattform specific code
+              |                                         |
++----------------------------+            +---------------------------+
+|     Tizen-ContentValues    |<-----------| TizenFactoryContentValues |
++----------------------------+            +---------------------------+
+*/
+class FactoryContentValues
+{
+private:
+  // a Instance of implemented plattform specific factory
+  // which has to be set in plattform specific code
+  static FactoryContentValues* instance;
+public:
+  // has to be called in plattform specific code
+  static void SetInstance(FactoryContentValues* plattformSpecific) { FactoryContentValues::instance = plattformSpecific; }
+  static FactoryContentValues* GetInstance()
+  {
+    if (FactoryContentValues::instance == nullptr)
+      throw;
+    return FactoryContentValues::instance;
+  }
+  // interface
+  virtual OsIndependentContentValues* CreateNewContentValues() = 0;
+};
