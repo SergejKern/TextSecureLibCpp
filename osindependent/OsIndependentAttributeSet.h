6799930b943b5c08767e36a824f38d170c961377
Sergej Kern <sk@roboterwerke.eu>
2015-10-07 14:46:40 +0200

    Update OsIndependentAttributeSet.h

--- osindependent/OsIndependentAttributeSet.h
+++ osindependent/OsIndependentAttributeSet.h
@@ -7,6 +7,8 @@
 // [ ] done
 // TFS ID: 667
 
+#include "..\Factory\Factory.h"
+
 /* public interface AttributeSet { */
 class OsIndependentAttributeSet
 {
@@ -15,4 +17,40 @@ public:
   /*
   */
   
+};
+/*
+Plattform independend Factory abstract class.
+This class must be implemented for specific plattforms, to create plattform specific String-classes
+
++---------------------------+  creates   +--------------------------+
+| OsIndependentAttributeSet |<-----------|    FactoryAttributeSet   |
++---------------------------+            +--------------------------+
+              ^                                 ^                Plattform independent code
+             /|\                               /|\
+              |                                 |
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+              |                                 |
+              |                                 |                Plattform specific code
+              |                                 |
++---------------------------+  creates   +--------------------------+
+|     Tizen-AttributeSet    |<-----------| TizenFactoryAttributeSet |
++---------------------------+            +--------------------------+
+*/
+class FactoryAttributeSet
+{
+private:
+  // a Instance of implemented plattform specific factory
+  // which has to be set in plattform specific code
+  static FactoryAttributeSet* instance;
+public:
+  // has to be called in plattform specific code
+  static void SetInstance(FactoryAttributeSet* plattformSpecific) { FactoryAttributeSet::instance = plattformSpecific; }
+  static FactoryAttributeSet* GetInstance()
+  {
+    if (FactoryAttributeSet::instance == nullptr)
+      throw;
+    return FactoryAttributeSet::instance;
+  }
+  // interface
+  virtual OsIndependentAttributeSet* CreateNewAttributeSet() = 0;
 };
