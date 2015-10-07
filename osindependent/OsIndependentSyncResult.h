e4aaa07999e5301428232eae58890980de1418af
Sergej Kern <sk@roboterwerke.eu>
2015-10-07 15:46:59 +0200

    Update OsIndependentSyncResult.h

--- osindependent/OsIndependentSyncResult.h
+++ osindependent/OsIndependentSyncResult.h
@@ -7,11 +7,48 @@
 // [ ] done
 // TFS ID: 686
 
-#include "OsIndependentParcelable.h"
+#include "..\Factory\Factory.h"
 
 // public class Account implements Parcelable {
 class OsIndependentSyncResult : OsIndependentParcelable
 {
 private:
+  OsIndependentSyncResult();
 public:
+};
+/*
+Plattform independend Factory abstract class.
+This class must be implemented for specific plattforms, to create plattform specific String-classes
+
++-------------------------+  creates   +------------------------+
+| OsIndependentSyncResult |<-----------|    FactorySyncResult   |
++-------------------------+            +------------------------+
+            ^                                      ^                Plattform independent code
+           /|\                                    /|\
+            |                                      |
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+            |                                      |
+            |                                      |                Plattform specific code
+            |                                      |
++-------------------------+  creates   +------------------------+
+|     Tizen-SyncResult    |<-----------| TizenFactorySyncResult |
++-------------------------+            +------------------------+
+*/
+class FactorySyncResult
+{
+private:
+  // a Instance of implemented plattform specific factory
+  // which has to be set in plattform specific code
+  static FactorySyncResult* instance;
+public:
+  // has to be called in plattform specific code
+  static void SetInstance(FactorySyncResult* plattformSpecific) { FactorySyncResult::instance = plattformSpecific; }
+  static FactorySyncResult* GetInstance()
+  {
+    if (FactorySyncResult::instance == nullptr)
+      throw;
+    return FactorySyncResult::instance;
+  }
+  // interface
+  virtual OsIndependentSyncResult* CreateNewSyncResult() = 0;
 };
