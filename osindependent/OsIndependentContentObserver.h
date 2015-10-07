590ea56a5b7e6fa82bd9089700cd8c673a67a4de
Sergej Kern <sk@roboterwerke.eu>
2015-10-07 15:57:52 +0200

    Update OsIndependentContentObserver.h

--- osindependent/OsIndependentContentObserver.h
+++ osindependent/OsIndependentContentObserver.h
@@ -1,16 +1,23 @@
 #pragma once
 /*
-  abstract class OsIndependentIntent to replace android.content.ContentResolver
-  http://grepcode.com/file/repository.grepcode.com/java/ext/com.google.android/android/5.1.1_r1/android/content/ContentResolver.java#ContentResolver
+  abstract class OsIndependentIntent to replace android.database.ContentObserver
+  http://grepcode.com/file/repository.grepcode.com/java/ext/com.google.android/android/5.1.1_r1/android/database/ContentObserver.java#ContentObserver
 */
 
 // [ ] done
 // TFS ID: 678
 
-/* public class  Intent implements Parcelable, Cloneable { */
+#include "..\Factory\Factory.h"
+
+/* public abstract class  [More ...] ContentObserver { */
 class OsIndependentContentObserver
 {
 private:
+  OsIndependentContentObserver();
 public:
 
 };
+
+/*
+needs no Factory, because is abstract
+*/
