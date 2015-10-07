4a46e2c4aa2b784c3b289544b519c5ec2bf7439c
Sergej Kern <sk@roboterwerke.eu>
2015-10-07 14:32:12 +0200

    Update OsIndependentSQLiteCursorDriver.h

--- osindependent/OsIndependentSQLiteCursorDriver.h
+++ osindependent/OsIndependentSQLiteCursorDriver.h
@@ -4,7 +4,7 @@
   http://grepcode.com/file/repository.grepcode.com/java/ext/com.google.android/android/5.1.1_r1/android/database/sqlite/SQLiteCursorDriver.java#SQLiteCursorDriver
 */
 
-// [x] done
+// [ ] done
 // TFS ID: 781
 
 #include "..\Factory\Factory.h"
@@ -34,4 +34,42 @@ public:
   bindArgs -> the new arguments
   */
   virtual void SetBindArguments(List<OsIndependentString*>* bindArgs) = 0;
+};
+/*
+Plattform independend Factory abstract class.
+This class must be implemented for specific plattforms, to create plattform specific String-classes
+
++---------------------------------+  creates   +--------------------------------+
+| OsIndependentSQLiteCursorDriver |<-----------|    FactorySQLiteCursorDriver   |
++---------------------------------+            +--------------------------------+
+                ^                                               ^                Plattform independent code
+               /|\                                             /|\
+                |                                               |
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+                |                                               |
+                |                                               |                Plattform specific code
+                |                                               |
++---------------------------------+  creates   +--------------------------------+
+|     Tizen-SQLiteCursorDriver    |<-----------| TizenFactorySQLiteCursorDriver |
++---------------------------------+            +--------------------------------+
+
+
+*/
+class FactorySQLiteCursorDriver
+{
+private:
+  // a Instance of implemented plattform specific factory
+  // which has to be set in plattform specific code
+  static FactorySQLiteCursorDriver* instance;
+public:
+  // has to be called in plattform specific code
+  static void SetInstance(FactorySQLiteCursorDriver* plattformSpecific) { FactorySQLiteCursorDriver::instance = plattformSpecific; }
+  static FactorySQLiteCursorDriver* GetInstance()
+  {
+    if (FactorySQLiteCursorDriver::instance == nullptr)
+      throw;
+    return FactorySQLiteCursorDriver::instance;
+  }
+  // interface
+  virtual OsIndependentSQLiteCursorDriver* CreateNewCursorDriver() = 0;
 };
