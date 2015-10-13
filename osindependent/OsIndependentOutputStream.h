c5aa7b59190b61683615df52beb63f0413a1a94f
Sergej Kern <sk@roboterwerke.eu>
2015-10-13 16:47:49 +0200

    Update OsIndependentOutputStream.h

--- osindependent/OsIndependentOutputStream.h
+++ osindependent/OsIndependentOutputStream.h
@@ -7,9 +7,9 @@
 // [x] done
 // TFS ID: 681
 
+#include <vector>
 #include "..\Factory\Factory.h"
 #include "..\javastuff\Closeable.h"
-#include "..\owntemplates\ArrayList.h"
 
 /*
 public abstract class OutputStream implements Closeable, Flushable {
@@ -21,9 +21,9 @@ public:
   // public abstract void  [More ...] write(int b) throws IOException;
   virtual void Write(int b) = 0;
   // public void[More ...] write(byte b[]) throws IOException{
-  virtual void Write(ArrayList<char>* b) = 0;
+  virtual void Write(std::vector<unsigned char>* b) = 0;
   // public void  [More ...] write(byte b[], int off, int len) throws IOException {
-  virtual void Write(ArrayList<char>* b, int off, int len) = 0;
+  virtual void Write(std::vector<unsigned char>* b, int off, int len) = 0;
   // public void  [More ...] flush() throws IOException {
   virtual void Flush() = 0;
   // public void  [More ...] close() throws IOException {
