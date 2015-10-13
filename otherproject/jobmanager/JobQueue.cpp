fb8e278ac76d3aae9c1f437e7e84e7a6bcd31add
Sergej Kern <sk@roboterwerke.eu>
2015-10-13 14:37:17 +0200

    Update JobQueue.cpp

--- otherproject/jobmanager/JobQueue.cpp
+++ otherproject/jobmanager/JobQueue.cpp
@@ -7,19 +7,23 @@ void JobQueue::OnRequirementStatusChanged()
 /*synchronized*/
 void JobQueue::Add(Job* job)
 {
-  jobQueue->Add(job);
+  jobQueue->push_back(job);
   NotifyAll();
 }
 /*synchronized*/
-void JobQueue::AddAll(List<Job*>* jobs)
+void JobQueue::AddAll(std::list<Job*>* jobs)
 {
-  jobQueue->AddAll(jobs);
+  //for (std::list<Job*>::iterator it = jobs->begin(); it != jobs->end(); ++it)
+  //{
+  //  jobQueue->push_back(*it);
+  //}
+  jobQueue->merge(*jobs);
   NotifyAll();
 }
 /*synchronized*/
 void JobQueue::Push(Job* job)
 {
-  jobQueue->AddFirst(job);
+  jobQueue->push_front(job);
 }
 /*synchronized*/
 Job* JobQueue::GetNext()
@@ -33,11 +37,16 @@ Job* JobQueue::GetNext()
     }
     return nextAvailableJob;
   }
-  catch (InterruptedException e)
+  catch(...) // (InterruptedException e) is thrown by java.lang.Object.wait()
   {
-    throw new AssertionError(e);
+    throw new AssertionError(/*e*/);
   }
 }
+bool JobQueue::IsGroupIdAvailable(OsIndependentString* groupId)
+{
+  return groupId == nullptr || !activeGroupIds->Contains(groupId);
+}
+// SetGroupIdAvailable mixed with SetGroupIdUnavailable ???
 /*synchronized*/ void JobQueue::SetGroupIdAvailable(OsIndependentString* groupId)
 {
   if (groupId != nullptr)
@@ -46,32 +55,29 @@ Job* JobQueue::GetNext()
     NotifyAll();
   }
 }
-Job* JobQueue::GetNextAvailableJob()
-{
-  if (jobQueue->IsEmpty())
-    return nullptr;
-
-  ListIterator<Job*> iterator = jobQueue->listIterator();
-  while (iterator.hasNext())
-  {
-    Job* job = iterator.next();
-    if (job->IsRequirementsMet() && IsGroupIdAvailable((OsIndependentString*)job->GetGroupId()))
-    {
-      iterator.remove();
-      SetGroupIdUnavailable((OsIndependentString*)job->GetGroupId());
-      return job;
-    }
-  }
-  return nullptr;
-}
-bool JobQueue::IsGroupIdAvailable(OsIndependentString* groupId)
-{
-  return groupId == nullptr || !activeGroupIds->Contains(groupId);
-}
+// SetGroupIdAvailable mixed with SetGroupIdUnavailable ???
 void JobQueue::SetGroupIdUnavailable(OsIndependentString* groupId)
 {
   if (groupId != nullptr)
   {
     activeGroupIds->Add(groupId);
   }
+}
+Job* JobQueue::GetNextAvailableJob()
+{
+  if (jobQueue->empty())
+    return nullptr;
+
+  for (std::list<Job*>::iterator it = jobQueue->begin(); it != jobQueue->end(); ++it)
+  {
+    Job* job = *it;
+    if (job->IsRequirementsMet() && IsGroupIdAvailable((OsIndependentString*)job->GetGroupId()))
+    {
+      // TODO iterator may break here!!!
+      jobQueue->remove(job);
+      SetGroupIdUnavailable((OsIndependentString*)job->GetGroupId());
+      return job;
+    }
+  }
+  return nullptr;
 }
