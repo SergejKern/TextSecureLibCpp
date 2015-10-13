421ff8234d13e878cef43d81b0532e6603191b0b
Sergej Kern <sk@roboterwerke.eu>
2015-10-13 13:58:40 +0200

    Create OsIndependentBroadcastReceiver.h

---
+++ osindependent/OsIndependentBroadcastReceiver.h
@@ -0,0 +1,25 @@
+#pragma once
+/*
+  abstract class OsIndependentCursor to replace android.content.BroadcastReceiver
+  http://grepcode.com/file/repository.grepcode.com/java/ext/com.google.android/android/5.1.1_r1/android/content/BroadcastReceiver.java#BroadcastReceiver
+*/
+
+// [ ] done
+// TFS ID: 797
+
+#include "..\Factory\Factory.h"
+
+/*
+public abstract class  [More ...] BroadcastReceiver {
+*/
+class OsIndependentBroadcastReceiver
+{
+public:
+public:
+  // public abstract void[More ...] onReceive(Context context, Intent intent);
+  virtual void OnReceive(OsIndependentContext* context, OsIndependentIntent* intent) = 0;
+};
+
+/*
+needs no Factory, because is abstract
+*/
